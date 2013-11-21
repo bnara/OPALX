b84107ee80f1798d2dc039fb935e81756c9267cc
Andreas Adelmann <andreas.adelmann@psi.ch>
2013-11-21 09:22:41 +0000

    new date

--- src/IpplVersions.h
+++ src/IpplVersions.h
@@ -7,8 +7,8 @@
 const char *ippl_version_name = "V1.0;01-2007;Linux;GNU";
 const char *ippl_compile_arch = "";
 const char *ippl_compile_line = "-ftemplate-depth-80 -funroll-loops -fstrict-aliasing -Wall -DIPPL_DEBUG  -DIPPL_LINUX -DIPPL_STRINGSTREAM -std=c++0x -DIPPL_MPI -DMPICH_SKIP_MPICXX -DIPPL_GCC -DIPPL_DONT_POOL -DIPPL_USE_XDIV_RNG -DIPPL_NO_STRINGSTREAM -DPETE_BITWISE_COPY -DIPPL_HAS_TEMPLATED_COMPLEX -DIPPL_USE_STANDARD_HEADERS -DIPPL_USE_PARTIAL_SPECIALIZATION -DIPPL_STDSTL -DIPPL_LONGLONG -DWITH_BRICK -DnoCOMP_GNUOLD -Drestrict=__restrict__ -DNOCTAssert   ";
-const char *ippl_compile_date = "Tue Jul 16 12:31:16 CEST 2013";
-const char *ippl_compile_machine = "Darwin dude.psi.ch 12.3.0 Darwin Kernel Version 12.3.0: Sun Jan  6 22:37:10 PST 2013; root:xnu-2050.22.13~1/RELEASE_X86_64 x86_64";
+const char *ippl_compile_date = "Fri Aug 30 06:46:15 CEST 2013";
+const char *ippl_compile_machine = "Darwin dude.psi.ch 12.4.0 Darwin Kernel Version 12.4.0: Wed May  1 17:57:12 PDT 2013; root:xnu-2050.24.15~1/RELEASE_X86_64 x86_64";
 const char *ippl_compile_options = "";
 const char *ippl_compile_user = "adelmann";
 const char *ippl_version_log[] = {
