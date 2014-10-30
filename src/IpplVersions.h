c06e8b1e2331c4378ded10b130e5fe709c850529
Andreas Adelmann <andreas.adelmann@psi.ch>
2014-10-30 21:17:56 +0000

    cleanup

---
+++ src/IpplVersions.h
@@ -0,0 +1,21 @@
+// Configuration and version information
+// generated automatically - do not edit
+
+#ifndef IPPL_VERSIONS_H
+#define IPPL_VERSIONS_H
+
+const char *ippl_version_name = "V1.0;01-2007;Linux;GNU";
+const char *ippl_compile_arch = "";
+const char *ippl_compile_line = "-ftemplate-depth-80 -funroll-loops -fstrict-aliasing -Wall -DIPPL_DEBUG  -DIPPL_LINUX -DIPPL_STRINGSTREAM -std=c++1y -DIPPL_MPI -DMPICH_SKIP_MPICXX -DIPPL_GCC -DIPPL_DONT_POOL -DIPPL_USE_XDIV_RNG -DIPPL_NO_STRINGSTREAM -DPETE_BITWISE_COPY -DIPPL_HAS_TEMPLATED_COMPLEX -DIPPL_USE_STANDARD_HEADERS -DIPPL_USE_PARTIAL_SPECIALIZATION -DIPPL_STDSTL -DIPPL_LONGLONG -DWITH_BRICK -DnoCOMP_GNUOLD -Drestrict=__restrict__ -DNOCTAssert   ";
+const char *ippl_compile_date = "Thu Oct 30 22:16:50 CET 2014";
+const char *ippl_compile_machine = "Darwin psivpn047.psi.ch 13.3.0 Darwin Kernel Version 13.3.0: Tue Jun  3 21:27:35 PDT 2014; root:xnu-2422.110.17~1/RELEASE_X86_64 x86_64";
+const char *ippl_compile_options = "";
+const char *ippl_compile_user = "adelmann";
+
+const char *ippl_version_log[] = {
+    ""
+};
+const unsigned int ippl_version_numfiles = 0;
+
+
+#endif
--- src/config.h
+++ src/config.h
@@ -7,9 +7,6 @@
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "IPPL"
 
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "IPPL 1.0"
-
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ippl"
 
