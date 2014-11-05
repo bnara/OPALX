0dddac37786dbccc411728e1e19f50f8e9fe008c
Andreas Adelmann <andreas.adelmann@psi.ch>
2014-11-05 06:46:41 +0000

    bring TestRC back on line

--- test/FFT/TestRC.cpp
+++ test/FFT/TestRC.cpp
@@ -20,195 +20,193 @@ using namespace std;
 #include <complex.h>
 #endif
 
+
+bool Configure(int argc, char *argv[],
+	       unsigned int *nx, unsigned int *ny, unsigned int *nz,
+	       int *serialDim, unsigned int *processes,  unsigned int *nLoop) 
+{
+
+  Inform msg("Configure ");
+  Inform errmsg("Error ");
+
+  string bc_str;
+  string dist_str;
+
+  for (int i=1; i < argc; ++i) {
+    string s(argv[i]);
+    if (s == "-grid") {
+      *nx = atoi(argv[++i]);
+      *ny = atoi(argv[++i]);
+      *nz = atoi(argv[++i]);
+    } else if (s == "-Loop") {
+      *nLoop = atoi(argv[++i]);
+    } else if (s == "-Decomp") {
+      *serialDim = atoi(argv[++i]);
+    } 
+    else {
+      errmsg << "Illegal format for or unknown option '" << s.c_str() << "'.";
+      errmsg << endl;
+    }
+  }
+  if (*serialDim == 0)
+    msg << "Serial dimension is x" << endl;
+  else if (*serialDim == 1)
+    msg << "Serial dimension is y" << endl;
+  else if (*serialDim == 2)
+    msg << "Serial dimension is z" << endl;
+  else {
+    *serialDim = 0;
+    msg << "Serial dimension is x" << endl;
+  }
+  *processes = Ippl::getNodes();
+
+  return true;
+}
+
+
+
+
+
+
 int main(int argc, char *argv[])
 {
   
   Ippl ippl(argc,argv);
   Inform testmsg(NULL,0);
 
-  // Hardwire dimensionality by setting cpp definition ONED, TWOD, or THREED:
-  // The preceding cpp definition causes compile-time setting of D:
   const unsigned D=3U;
+  bool compressTemps = false;
+  bool constInput    = true;  // preserve input field in two-field transform
+
   testmsg << "%%%%%%% Dimensionality: D = " << D << " %%%%%%%" << endl;
   
+
+  unsigned int processes;
+  int serialDim;
+  unsigned int nx,ny,nz;
+  unsigned int nLoop;
+
+  Configure(argc, argv, &nx, &ny, &nz, &serialDim, &processes, &nLoop); 
+
+  int vnodes = processes;
   unsigned ngrid[D];   // grid sizes
+  ngrid[0] = nx;
+  ngrid[1] = ny;
+  ngrid[2] = nz;
 
   // Used in evaluating correctness of results:
   double realDiff;
    
   // Various counters, constants, etc:
-  int d;
-  int Parent = 0;
-  int tag = Ippl::Comm->next_tag(IPPL_APP_TAG0);
+  
   double pi = acos(-1.0);
   double twopi = 2.0*pi;
+
   // Timer:
   Timer timer;
-  bool constInput;  // preserve input field in two-field transform
 
-  // Layout information:
-  unsigned vnodes;             // number of vnodes; input at cmd line
   e_dim_tag allParallel[D];    // Specifies SERIAL, PARALLEL dims
-  for (d=0; d<D; d++) 
+  for (unsigned int d=0; d<D; d++) 
     allParallel[d] = PARALLEL;
+
   e_dim_tag serialParallel[D]; // Specifies SERIAL, PARALLEL dims
-  serialParallel[0] = SERIAL;
-  for (d=1; d<D; d++) 
+  for (unsigned int d=0; d<D; d++) 
     serialParallel[d] = PARALLEL;
+  serialParallel[serialDim] = SERIAL;
 
-  // Compression of temporaries:
-  bool compressTemps;
-
-  // ================BEGIN INTERACTION LOOP====================================
-  while (true) {
-
-    // read in vnodes etc. off of node 0
-    if( Ippl::Comm->myNode() == Parent ) {
-      bool vnodesOK = false;
-      while (!vnodesOK) {
-        cout << "Enter vnodes (0 to exit): ";
-        cin >> vnodes;
-        if (vnodes==0 || vnodes>=Ippl::getNodes())
-          vnodesOK = true;
-        else
-          cout << "Number of vnodes should not be less than number of pnodes!"
-               << endl;
-      }
-      if (vnodes > 0) {
-	cout << "Compress temps? (Enter 1 for true or 0 false): ";
-	int tempWorkaroundBoolIO;
-	cin >> tempWorkaroundBoolIO;
-	if (tempWorkaroundBoolIO == 0)
-	  compressTemps = false;
-        else
-	  compressTemps = true;
-	cout << "Constant input fields? (Enter 1 for true or 0 false): ";
-	cin >> tempWorkaroundBoolIO;
-	if (tempWorkaroundBoolIO == 0)
-	  constInput = false;
-        else
-	  constInput = true;
-	cout << "input nx, ny, and nz; space-delimited: ";
-	for (d=0; d<D; d++) 
-	  cin >> ngrid[d];
-      }
-      // now broadcast data to other nodes
-      Message *mess = new Message();
-      putMessage( *mess, vnodes );
-      if (vnodes > 0) {
-	putMessage(*mess, compressTemps);
-	putMessage(*mess, constInput);
-	for (d=0; d<D; d++) putMessage( *mess, ngrid[d] );
-      }
-      Ippl::Comm->broadcast_all(mess, tag);
-    }
-    // now each node recieves the data
-    unsigned pe = Ippl::Comm->myNode();
-    Message *mess = Ippl::Comm->receive_block(Parent, tag);
-    PAssert(mess);
-    getMessage( *mess, vnodes );
-    if (vnodes <= 0) 
-      break;
-    getMessage(*mess, compressTemps);
-    getMessage(*mess, constInput);
-    for (d=0; d<D; d++) 
-      getMessage( *mess, ngrid[d] );
-    delete mess;
-    
-    // create standard domain
-    NDIndex<D> ndiStandard;
-    for (d=0; d<D; d++) 
-      ndiStandard[d] = Index(ngrid[d]);
-    // create new domain with axes permuted to match FFT output
-    
-    // create half-size domain for RC transform along zeroth axis
-    NDIndex<D> ndiStandard0h = ndiStandard;
-    ndiStandard0h[0] = Index(ngrid[0]/2+1);
+  // create standard domain
+  NDIndex<D> ndiStandard;
+  for (unsigned int d=0; d<D; d++) 
+    ndiStandard[d] = Index(ngrid[d]);
+  // create new domain with axes permuted to match FFT output
   
-     // all parallel layout, standard domain, normal axis order
-    FieldLayout<D> layoutPPStan(ndiStandard,allParallel,vnodes);
-    // zeroth axis serial, standard domain, normal axis order
-    FieldLayout<D> layoutSPStan(ndiStandard,serialParallel,vnodes);
- 
-    // all parallel layout, zeroth axis half-size domain, normal axis order
-    FieldLayout<D> layoutPPStan0h(ndiStandard0h,allParallel,vnodes);
-    // zeroth axis serial, zeroth axis half-size domain, normal axis order
-    FieldLayout<D> layoutSPStan0h(ndiStandard0h,serialParallel,vnodes);
-       
-    // create test Fields for complex-to-complex FFT
-    BareField<dcomplex,D> CFieldPPStan(layoutPPStan);
-   
-    BareField<dcomplex,D> CFieldSPStan(layoutSPStan);
-   
-    BareField<double,D> diffFieldSPStan(layoutSPStan);
-      
-    // create test Fields for real-to-complex FFT
-    BareField<double,D> RFieldSPStan(layoutSPStan);
-    BareField<double,D> RFieldSPStan_save(layoutSPStan);
-    BareField<dcomplex,D> CFieldSPStan0h(layoutSPStan0h);
- 
-    testmsg << "RFieldSPStan   layout= " << layoutSPStan << endl;
-    testmsg << "CFieldSPStan0h layout= " << layoutSPStan0h << endl;
-
-
-    // For calling FieldDebug functions from debugger, set up output format:
-    setFormat(4,3);
-
-    // Rather more complete test functions (sine or cosine mode):
-    dcomplex sfact(1.0,0.0);      // (1,0) for sine mode; (0,0) for cosine mode
-    dcomplex cfact(0.0,0.0);      // (0,0) for sine mode; (1,0) for cosine mode
-
-    double xfact, kx, yfact, ky, zfact, kz;
-    xfact = pi/(ngrid[0] + 1.0);
-    yfact = 2.0*twopi/(ngrid[1]);
-    zfact = 2.0*twopi/(ngrid[2]);
-    kx = 1.0; ky = 2.0; kz = 3.0; // wavenumbers
-
-    CFieldPPStan[ndiStandard[0]][ndiStandard[1]][ndiStandard[2]] = 
-      sfact * ( sin( (ndiStandard[0]+1) * kx * xfact +
-                      ndiStandard[1]    * ky * yfact +
-                      ndiStandard[2]    * kz * zfact ) +
-		sin( (ndiStandard[0]+1) * kx * xfact -
-                      ndiStandard[1]    * ky * yfact -
-                      ndiStandard[2]    * kz * zfact ) ) + 
-      cfact * (-cos( (ndiStandard[0]+1) * kx * xfact +
-                      ndiStandard[1]    * ky * yfact +
-                      ndiStandard[2]    * kz * zfact ) + 
-		cos( (ndiStandard[0]+1) * kx * xfact -
-                      ndiStandard[1]    * ky * yfact -
-                      ndiStandard[2]    * kz * zfact ) );
-      
-    // RC FFT tests
+  // create half-size domain for RC transform along zeroth axis
+  NDIndex<D> ndiStandard0h = ndiStandard;
+  ndiStandard0h[0] = Index(ngrid[0]/2+1);
   
-    RFieldSPStan = real(CFieldPPStan);
-    CFieldSPStan0h = dcomplex(0.0,0.0);
+  // all parallel layout, standard domain, normal axis order
+  FieldLayout<D> layoutPPStan(ndiStandard,allParallel,vnodes);
+  // zeroth axis serial, standard domain, normal axis order
+  FieldLayout<D> layoutSPStan(ndiStandard,serialParallel,vnodes);
+  
+  // all parallel layout, zeroth axis half-size domain, normal axis order
+  FieldLayout<D> layoutPPStan0h(ndiStandard0h,allParallel,vnodes);
+  // zeroth axis serial, zeroth axis half-size domain, normal axis order
+  FieldLayout<D> layoutSPStan0h(ndiStandard0h,serialParallel,vnodes);
+    
+  // create test Fields for complex-to-complex FFT
+  BareField<dcomplex,D> CFieldPPStan(layoutPPStan);
+  
+  BareField<dcomplex,D> CFieldSPStan(layoutSPStan);
+  
+  BareField<double,D> diffFieldSPStan(layoutSPStan);
+  
+  // create test Fields for real-to-complex FFT
+  BareField<double,D> RFieldSPStan(layoutSPStan);
+  BareField<double,D> RFieldSPStan_save(layoutSPStan);
+  BareField<dcomplex,D> CFieldSPStan0h(layoutSPStan0h);
+  
+  INFOMSG("RFieldSPStan   layout= " << layoutSPStan << endl;);
+  INFOMSG("CFieldSPStan0h layout= " << layoutSPStan0h << endl;);
+  
+  // For calling FieldDebug functions from debugger, set up output format:
+  setFormat(4,3);
+
+  // Rather more complete test functions (sine or cosine mode):
+  dcomplex sfact(1.0,0.0);      // (1,0) for sine mode; (0,0) for cosine mode
+  dcomplex cfact(0.0,0.0);      // (0,0) for sine mode; (1,0) for cosine mode
+  
+  double xfact, kx, yfact, ky, zfact, kz;
+  xfact = pi/(ngrid[0] + 1.0);
+  yfact = 2.0*twopi/(ngrid[1]);
+  zfact = 2.0*twopi/(ngrid[2]);
+  kx = 1.0; ky = 2.0; kz = 3.0; // wavenumbers
+
+  CFieldPPStan[ndiStandard[0]][ndiStandard[1]][ndiStandard[2]] = 
+    sfact * ( sin( (ndiStandard[0]+1) * kx * xfact +
+		   ndiStandard[1]    * ky * yfact +
+		   ndiStandard[2]    * kz * zfact ) +
+	      sin( (ndiStandard[0]+1) * kx * xfact -
+		   ndiStandard[1]    * ky * yfact -
+		   ndiStandard[2]    * kz * zfact ) ) + 
+    cfact * (-cos( (ndiStandard[0]+1) * kx * xfact +
+		   ndiStandard[1]    * ky * yfact +
+		   ndiStandard[2]    * kz * zfact ) + 
+	     cos( (ndiStandard[0]+1) * kx * xfact -
+		  ndiStandard[1]    * ky * yfact -
+		  ndiStandard[2]    * kz * zfact ) );
+  
+  // RC FFT tests
+  
+  RFieldSPStan = real(CFieldPPStan);
+  CFieldSPStan0h = dcomplex(0.0,0.0);
    
+
+
+  // create RC FFT object
+  FFT<RCTransform,D,double> rcfft(ndiStandard, ndiStandard0h, compressTemps);
+
+  // set direction names
+  rcfft.setDirectionName(+1, "forward");
+  rcfft.setDirectionName(-1, "inverse");
+  
+  testmsg << "RC transform using layout with zeroth dim serial ..." << endl;
+  for (unsigned i=0; i<nLoop; i++) {
     RFieldSPStan_save = RFieldSPStan;
-
-    // create RC FFT object
-    FFT<RCTransform,D,double> rcfft(ndiStandard, ndiStandard0h, compressTemps);
-
-    // set direction names
-    rcfft.setDirectionName(+1, "forward");
-    rcfft.setDirectionName(-1, "inverse");
-
-    //-------------------------------------------------------------------------
-
-    testmsg << "RC transform using layout with zeroth dim serial ..." << endl;
     timer.start();
-    
     rcfft.transform("forward", RFieldSPStan,  CFieldSPStan0h, constInput);
     rcfft.transform("inverse", CFieldSPStan0h, RFieldSPStan, constInput);
     timer.stop();
-
+  
     diffFieldSPStan = Abs(RFieldSPStan - RFieldSPStan_save);
     realDiff = max(diffFieldSPStan);
     testmsg << "fabs(realDiff) = " << fabs(realDiff) << endl;
-    testmsg << "CPU time used = " << timer.cpu_time() << " secs." << endl;
-    timer.clear();
-    // =================END INTERACTION LOOP=====================================
-    return 0;
   }
+  
+  testmsg << "CPU time used = " << timer.cpu_time() << " secs." << endl;
+  timer.clear();
+
+  return 0;
 }
 /***************************************************************************
  * $RCSfile: TestRC.cpp,v $   $Author: adelmann $
