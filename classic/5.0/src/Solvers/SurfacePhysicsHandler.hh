0060bdd6919d9c221afb71f7a967ccee97c068b6
Uldis Locans <locans.uldis@psi.ch>
2016-02-15 13:37:19 +0100

    add functions to get name of surfacephysicshandler and number of particles inside surfacephysics handler, added parameter numParticlesInSimulation to apply function, needed to correctly allocate GPU memory

--- classic/5.0/src/Solvers/SurfacePhysicsHandler.hh
+++ classic/5.0/src/Solvers/SurfacePhysicsHandler.hh
@@ -11,11 +11,13 @@ class SurfacePhysicsHandler {
 public:
     SurfacePhysicsHandler(std::string name, ElementBase *elref);
     virtual ~SurfacePhysicsHandler() { };
-    virtual void apply(PartBunch &bunch) = 0;
+    virtual void apply(PartBunch &bunch, size_t numParticlesInSimulation = 0) = 0;
     virtual const std::string getType() const = 0;
     virtual void print(Inform& os) = 0;
     virtual bool stillActive() = 0;
     virtual double getTime() = 0;
+    virtual std::string getName() = 0;
+    virtual size_t getParticlesInMat() = 0;
     void AllParticlesIn(bool p);
     void updateElement(ElementBase *newref);
   
