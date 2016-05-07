f99999adfe2f55d8d8c248e781291cb27e54b7ae
Andreas Adelmann <andreas,adelmann@psi.ch>
2016-05-07 02:43:18 +0200

    Make Tags mpi standard conform

--- ippl/src/Message/Tags.h
+++ ippl/src/Message/Tags.h
@@ -29,87 +29,87 @@
 
 
 // tags for reduction
-#define COMM_REDUCE_SEND_TAG    1000000
-#define COMM_REDUCE_RECV_TAG    1100000
-#define COMM_REDUCE_SCATTER_TAG 1200000
-#define COMM_REDUCE_CYCLE        100000
+#define COMM_REDUCE_SEND_TAG    10000
+#define COMM_REDUCE_RECV_TAG    11000
+#define COMM_REDUCE_SCATTER_TAG 12000
+#define COMM_REDUCE_CYCLE        1000
 
 
 // tag for applying parallel periodic boundary condition.
 
-#define BC_PARALLEL_PERIODIC_TAG 1500000
-#define BC_TAG_CYCLE              100000
+#define BC_PARALLEL_PERIODIC_TAG 15000
+#define BC_TAG_CYCLE              1000
 
 // Field<T,Dim> tags
-#define F_GUARD_CELLS_TAG       2000000 // Field::fillGuardCells()
-#define F_WRITE_TAG             2100000 // Field::write()
-#define F_READ_TAG              2200000 // Field::read()
-#define F_GEN_ASSIGN_TAG        2300000 // assign(BareField,BareField)
-#define F_REPARTITION_BCAST_TAG 2400000 // broadcast in FieldLayout::repartion.
-#define F_REDUCE_PERP_TAG       2500000 // reduction in binary load balance.
-#define F_GETSINGLE_TAG         2600000 // IndexedBareField::getsingle()
-#define F_REDUCE_TAG            2700000 // Reduction in minloc/maxloc
-#define F_LAYOUT_IO_TAG         2800000 // Reduction in minloc/maxloc
-#define F_TAG_CYCLE              100000
+#define F_GUARD_CELLS_TAG       20000 // Field::fillGuardCells()
+#define F_WRITE_TAG             21000 // Field::write()
+#define F_READ_TAG              22000 // Field::read()
+#define F_GEN_ASSIGN_TAG        23000 // assign(BareField,BareField)
+#define F_REPARTITION_BCAST_TAG 24000 // broadcast in FieldLayout::repartion.
+#define F_REDUCE_PERP_TAG       25000 // reduction in binary load balance.
+#define F_GETSINGLE_TAG         26000 // IndexedBareField::getsingle()
+#define F_REDUCE_TAG            27000 // Reduction in minloc/maxloc
+#define F_LAYOUT_IO_TAG         28000 // Reduction in minloc/maxloc
+#define F_TAG_CYCLE              1000
 
 // Tags for FieldView and FieldBlock
-#define FV_2D_TAG               3000000 // FieldView::update_2D_data()
-#define FV_3D_TAG               3100000 // FieldView::update_2D_data()
-#define FV_TAG_CYCLE             100000
+#define FV_2D_TAG               30000 // FieldView::update_2D_data()
+#define FV_3D_TAG               31000 // FieldView::update_2D_data()
+#define FV_TAG_CYCLE             1000
 
-#define FB_WRITE_TAG            3200000 // FieldBlock::write()
-#define FB_READ_TAG             3300000 // FieldBlock::read()
-#define FB_TAG_CYCLE             100000
+#define FB_WRITE_TAG            32000 // FieldBlock::write()
+#define FB_READ_TAG             33000 // FieldBlock::read()
+#define FB_TAG_CYCLE             1000
 
-#define FP_GATHER_TAG           3400000 // FieldPrint::print()
-#define FP_TAG_CYCLE             100000
+#define FP_GATHER_TAG           34000 // FieldPrint::print()
+#define FP_TAG_CYCLE             1000
 
 // Tags for DiskField
-#define DF_MAKE_HOST_MAP_TAG    3500000
-#define DF_FIND_RECV_NODES_TAG  3600000
-#define DF_QUERY_TAG            3700000
-#define DF_READ_TAG             3800000
-#define DF_OFFSET_TAG           3900000
-#define DF_READ_META_TAG        4000000
-#define DF_TAG_CYCLE             100000
+#define DF_MAKE_HOST_MAP_TAG    35000
+#define DF_FIND_RECV_NODES_TAG  36000
+#define DF_QUERY_TAG            37000
+#define DF_READ_TAG             38000
+#define DF_OFFSET_TAG           39000
+#define DF_READ_META_TAG        40000
+#define DF_TAG_CYCLE             1000
 
 // Special tags used by Particle classes for communication.
-#define P_WEIGHTED_LAYOUT_TAG   5000000
-#define P_WEIGHTED_RETURN_TAG   5100000
-#define P_WEIGHTED_TRANSFER_TAG 5200000
-#define P_SPATIAL_LAYOUT_TAG    5300000
-#define P_SPATIAL_RETURN_TAG    5400000
-#define P_SPATIAL_TRANSFER_TAG  5500000
-#define P_SPATIAL_GHOST_TAG     5600000
-#define P_SPATIAL_RANGE_TAG     5700000
-#define P_RESET_ID_TAG          5800000
-#define P_LAYOUT_CYCLE           100000
+#define P_WEIGHTED_LAYOUT_TAG   50000
+#define P_WEIGHTED_RETURN_TAG   51000
+#define P_WEIGHTED_TRANSFER_TAG 52000
+#define P_SPATIAL_LAYOUT_TAG    53000
+#define P_SPATIAL_RETURN_TAG    54000
+#define P_SPATIAL_TRANSFER_TAG  55000
+#define P_SPATIAL_GHOST_TAG     56000
+#define P_SPATIAL_RANGE_TAG     57000
+#define P_RESET_ID_TAG          58000
+#define P_LAYOUT_CYCLE           1000
 
 // Tags for Ippl setup
-#define IPPL_MAKE_HOST_MAP_TAG    6000000
-#define IPPL_TAG_CYCLE             100000
+#define IPPL_MAKE_HOST_MAP_TAG    60000
+#define IPPL_TAG_CYCLE             1000
 
 // Tags for Conejo load balancer
-#define F_CONEJO_BALANCER_TAG      7000000
-#define F_CB_BCAST_TAG             7100000
-#define F_CB_DOMAIN_TAG            7200000
+#define F_CONEJO_BALANCER_TAG      70000
+#define F_CB_BCAST_TAG             71000
+#define F_CB_DOMAIN_TAG            72000
 
 // Tags for VnodeMultiBalancer
-#define VNMB_PARTIAL_TAG           8000000
-#define VNMB_COMPLETE_TAG          8100000
-#define VNMB_TAG_CYCLE              100000
+#define VNMB_PARTIAL_TAG           80000
+#define VNMB_COMPLETE_TAG          81000
+#define VNMB_TAG_CYCLE              1000
 
 // Tags for Ippl application codes
-#define IPPL_APP_TAG0    9000000
-#define IPPL_APP_TAG1    9100000
-#define IPPL_APP_TAG2    9200000
-#define IPPL_APP_TAG3    9300000
-#define IPPL_APP_TAG4    9400000
-#define IPPL_APP_TAG5    9500000
-#define IPPL_APP_TAG6    9600000
-#define IPPL_APP_TAG7    9700000
-#define IPPL_APP_TAG8    9800000
-#define IPPL_APP_TAG9    9900000
-#define IPPL_APP_CYCLE    100000
+#define IPPL_APP_TAG0    90000
+#define IPPL_APP_TAG1    91000
+#define IPPL_APP_TAG2    92000
+#define IPPL_APP_TAG3    93000
+#define IPPL_APP_TAG4    94000
+#define IPPL_APP_TAG5    95000
+#define IPPL_APP_TAG6    96000
+#define IPPL_APP_TAG7    97000
+#define IPPL_APP_TAG8    98000
+#define IPPL_APP_TAG9    99000
+#define IPPL_APP_CYCLE    1000
 
 #endif // TAGS_H
