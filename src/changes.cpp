78c8a6e568d2170d18052afbd9b60584335a5b16
Christof Metzger-Kraus <christof.j.metzger@gmail.com>
2017-02-08 21:33:13 +0100

    adapt gitignore

--- src/changes.cpp
+++ src/changes.cpp
@@ -12,16 +12,17 @@ namespace Versions {
                     "  Instead of normalizing with the overall maximum value of longitudinal\n"
                     "  component Opal now uses the maximum value on axis.\n"
                     "\n"
-                    "* The design energy of dipoles is now expected to be in MeV instead of eV.\n"
+                    "* OPAL-T: The design energy of dipoles is now expected to be in MeV instead\n"
+                    "  of eV.\n"
                     });
 
         changes.insert({109,
-                    "* Beamlines are now placed in 3-dimensional space. Make sure that you\n"
-                    "  use apertures to limit the range of the elements. Default aperture \n"
+                    "* OPAL-T: Beamlines are now placed in 3-dimensional space. Make sure that\n"
+                    "  you use apertures to limit the range of the elements. Default aperture \n"
                     "  has circular shape with diameter 1 meter.\n"
                     "\n"
-                    "* Beamlines containing a cathode have to have a SOURCE element to indicate\n"
-                    "  this fact\n"
+                    "* OPAL-T: Beamlines containing a cathode have to have a SOURCE element to\n"
+                    "  indicate this fact\n"
                     });
     }
 }
