018ab1e070fcc5e82bdec6fe07fb85b40d4bf171
Matthias Frey <matthias.frey@psi.ch>
2017-02-10 17:25:50 +0100

    AMR test case: 2-stream instability
    - initialize field only once
    - increase number of guard cells for field (from 1 to 2)

--- ippl/test/AMR/ippl-amr/new-twostream.cpp
+++ ippl/test/AMR/ippl-amr/new-twostream.cpp
@@ -32,152 +32,26 @@ typedef Vektor<double, BL_SPACEDIM> Vector_t;
 
 #include "Particle/BoxParticleCachingPolicy.h"
 #define Dim 3
-typedef UniformCartesian<Dim, double>                                 Mesh_t;
-typedef BoxParticleCachingPolicy<double, Dim, Mesh_t>                 CachingPolicy_t;
-typedef ParticleSpatialLayout<double, Dim, Mesh_t, CachingPolicy_t>   playout_t;
 typedef Cell                                                          Center_t;
-typedef CenteredFieldLayout<Dim, Mesh_t, Center_t>                    FieldLayout_t;
-typedef Field<double, Dim, Mesh_t, Center_t>                          Field_t;
-
 typedef IntCIC                                                        IntrplCIC_t;
-
 typedef UniformCartesian<2, double>                                   Mesh2d_t;
 typedef CenteredFieldLayout<2, Mesh2d_t, Center_t>                    FieldLayout2d_t;
 typedef Field<double, 2, Mesh2d_t, Center_t>                          Field2d_t;
 
-template<class PL>
-class TempParticle : public IpplParticleBase<PL> {
+void ipplProjection(Field2d_t& field,
+                    const Vektor<double, 3>& dx,
+                    const Vektor<double, 3>& dv,
+                    const Vektor<double, 3>& Vmax,
+                    const NDIndex<2>& lDom,
+                    amrbunch_t* bunch, int step)
+{
+    field = 0; // otherwise values are accumulated over steps
     
-public:
-    ParticleAttrib<double>     	Q;
-    ParticleAttrib<Vektor<double,2> > Rphase; //velocity of the particles
-    ParticleAttrib<Vector_t>	v; //velocity of the particles
-    
-    TempParticle(PL* pl,
-                 Vektor<double,3> nr,
-                 e_dim_tag decomp[Dim],
-                 Vektor<double,3> extend_l_,
-                 Vektor<double,3> extend_r_,
-                 Vektor<int,3> Nx_,
-                 Vektor<int,3> Nv_,
-                 Vektor<double,3> Vmax_) :
-        IpplParticleBase<PL>(pl),
-        nr_m(nr),
-        extend_l(extend_l_),
-        extend_r(extend_r_),
-        Nx(Nx_), Nv(Nv_), Vmax(Vmax_)
-    {
-        this->addAttribute(Q);
-        this->addAttribute(Rphase);
-        this->addAttribute(v);
-        
-        double spacings[2] = {(extend_r[2]-extend_l[2])/(Nx[2]),2.*Vmax[2]/(Nv[2])};
-        Vektor<double,2> origin;
-        origin(0) = extend_l[2]; origin(1) = -Vmax[2]; 
-        Index I(Nx[2]+1); Index J(Nv[2]+1);
-        domain2d_m[0]=I; domain2d_m[1]=J;
-
-        mesh2d_m=Mesh2d_t(domain2d_m, spacings, origin);
-        layout2d_m = new FieldLayout2d_t(mesh2d_m);
-        
-        //set origin and spacing is needed for correct results, even if mesh was created with these paremeters ?!
-        mesh2d_m.set_meshSpacing(&(spacings[0]));
-        mesh2d_m.set_origin(origin);
-
-        domain2d_m = layout2d_m->getDomain();
-        //f_m is used for twostream instability as 2D phase space mesh
-        
-        BConds<double,2,UniformCartesian<2,double>,Cell> BC;
-        if (Ippl::getNodes()>1) {
-            BC[0] = new ParallelInterpolationFace<double,2,Mesh2d_t,Cell>(0);
-            BC[1] = new ParallelInterpolationFace<double,2,Mesh2d_t,Cell>(1);
-            BC[2] = new ParallelInterpolationFace<double,2,Mesh2d_t,Cell>(2);
-            BC[3] = new ParallelInterpolationFace<double,2,Mesh2d_t,Cell>(3);
-        }
-        else {
-            BC[0] = new InterpolationFace<double,2,Mesh2d_t,Cell>(0);
-            BC[1] = new InterpolationFace<double,2,Mesh2d_t,Cell>(1);
-            BC[2] = new InterpolationFace<double,2,Mesh2d_t,Cell>(2);
-            BC[3] = new InterpolationFace<double,2,Mesh2d_t,Cell>(3);
-        }
-        
-        f_m.initialize(mesh2d_m, *layout2d_m, GuardCellSizes<2>(1),BC);
-    }
-    
-    void interpolate_distribution(Vektor<double,3> dx, Vektor<double,3> dv){
-        f_m=0;
-        for (unsigned i=0; i<this->getLocalNum(); ++i) {
-//             SpaceQ[i]=Q[i]/(dx[0]*dx[1]*dv[0]*dv[1]);
-            Rphase[i]=Vektor<double,2>(this->R[i][2],v[i][2]);
-        }
-        //this->SpaceQ.scatter(this->f_m, this->Rphase, IntrplCIC_t());
-        this->Q.scatter(this->f_m, this->Rphase, IntrplCIC_t());
-    }
-    
-public:
-    Vektor<int,Dim> nr_m;
-    Vektor<double,Dim> extend_l;
-    Vektor<double,Dim> extend_r;
-    Vektor<int,Dim> Nx;
-    Vektor<int,Dim> Nv; 
-    Vektor<double,Dim> Vmax;
-    //Fields for tracking distribution function
-    Field2d_t f_m;
-    Mesh2d_t mesh2d_m;
-    NDIndex<2> domain2d_m;	
-    FieldLayout2d_t *layout2d_m;
-    
-};
-
-void ipplProjection(Vektor<int,Dim> nr,
-                    Vektor<double,Dim> extend_l,
-                    Vektor<double,Dim> extend_r,
-                    Vektor<int,Dim> Nx,
-                    Vektor<int,Dim> Nv,
-                    Vektor<double,Dim> Vmax,
-                    amrbunch_t* bunch, int step) {
-    e_dim_tag decomp[Dim];
-    Mesh_t *mesh;
-    FieldLayout_t *FL;
-
-    NDIndex<Dim> domain;
-    for (unsigned i=0; i<Dim; i++)
-        domain[i] = domain[i] = Index(nr[i]+1);
-
-    for (unsigned d=0; d < Dim; ++d)
-        decomp[d] = PARALLEL;
-    
-    // create mesh and layout objects for this problem domain
-    mesh          = new Mesh_t(domain);
-    FL            = new FieldLayout_t(*mesh, decomp);
-    playout_t* PL = new playout_t(*FL, *mesh);
-    
-    TempParticle<playout_t>  *P = new TempParticle<playout_t>(PL, nr, decomp, extend_l, extend_r,Nx,Nv,Vmax);
-    
-    //**************
-    // copy the particles to Ippl container
-    for (std::size_t i = 0; i < bunch->getLocalNum(); ++i) {
-        P->create(1);
-        P->Q[i] = bunch->qm[i];
-        P->v[i] = bunch->P[i];
-        P->R[i] = bunch->R[i];
-    }
-    //**************
-    
-    
-    P->interpolate_distribution((extend_r-extend_l)/(Nx),2.*Vmax/(Nv));
-    
-    /*
     for (unsigned i=0; i< bunch->getLocalNum(); ++i)
-      bunch->Rphase[i]= Vektor<double,2>(bunch->R[i][2],bunch->P[i][2]);
+        bunch->Rphase[i]= Vektor<double,2>(bunch->R[i][2],bunch->P[i][2]);
     
-    bunch->qm.scatter(P->f_m, bunch->Rphase, IntrplCIC_t());
-    */
-
-    // ---------------
-    Vektor<double,3> dx = (P->extend_r-P->extend_l)/(P->Nx);
+    bunch->qm.scatter(field, bunch->Rphase, IntrplCIC_t());
     
-    Vektor<double,3> dv = 2.*P->Vmax/(P->Nv);
     std::ofstream csvout;
     csvout.precision(10);
     csvout.setf(std::ios::scientific, std::ios::floatfield);
@@ -191,25 +65,18 @@ void ipplProjection(Vektor<int,Dim> nr,
     // and start with header
     csvout.open(fname.str().c_str(), std::ios::out);
     csvout << "z, vz, f" << std::endl;
-    NDIndex<2> lDom = P->domain2d_m;
     
     for (int i=lDom[0].first(); i<=lDom[0].last(); i++) {
     
         for (int j=lDom[1].first(); j<=lDom[1].last(); j++) {
         
-            csvout << (i+0.5)*dx[2] << ","
-                   << (j+0.5)*dv[2]-P->Vmax[2]
-                   << "," << P->f_m[i][j].get() << std::endl;
+            csvout << (i+0.5) * dx[2] << ","
+                   << (j+0.5) * dv[2] - Vmax[2]
+                   << "," << field[i][j].get() << std::endl;
         }
     }
-    // close the output file for this iteration:
-    csvout.close();
-    // -------------------
     
-    delete P;
-//     delete PL;
-    delete FL;
-    delete mesh;
+    csvout.close();
 }
 
 // ------------------------------------------------------------------------------------------
@@ -341,11 +208,19 @@ void doTwoStream(Vektor<std::size_t, 3> nr,
     
     // initialize a particle distribution
     Distribution dist;
-    dist.twostream(Vektor<double, 3>(lower[0], lower[1], lower[2]),
-                   Vektor<double, 3>(upper[0], upper[1], upper[2]),
-                   Vektor<std::size_t, 3>(4, 4, 32),
-                   Vektor<std::size_t, 3>(8, 8, 128),
-                   Vektor<double, 3>(6.0, 6.0, 6.0), 0.05);
+    
+    Vektor<double, 3> extend_l = Vektor<double, 3>(lower[0], lower[1], lower[2]);
+    Vektor<double, 3> extend_r = Vektor<double, 3>(upper[0], upper[1], upper[2]);
+    Vektor<std::size_t, 3> Nx = Vektor<std::size_t, 3>(4, 4, 32);
+    Vektor<std::size_t, 3> Nv = Vektor<std::size_t, 3>(8, 8, 128);
+    Vektor<double, 3> Vmax = Vektor<double, 3>(6.0, 6.0, 6.0);
+    
+    dist.twostream(extend_l,
+                   extend_r,
+                   Nx,
+                   Nv,
+                   Vmax,
+                   0.05);
     
     // copy particles to the PartBunchAmr object.
     dist.injectBeam( *(bunch.get()) );
@@ -363,14 +238,67 @@ void doTwoStream(Vektor<std::size_t, 3> nr,
     msg << "Multi-level statistics" << endl;
     bunch->gatherStatistics();
     
-    for (std::size_t i = 0; i < nIter; ++i) {
+    
+    // --------------------------------------------------------------------
+    
+    double spacings[2] = {
+        ( extend_r[2] - extend_l[2] ) / Nx[2],
+        2. * Vmax[2] / Nv[2]
+    };
+    
+    Vektor<double,2> origin = {
+        extend_l[2],
+        -Vmax[2]
+    };
+    
+    Index I(Nx[2]+1);
+    Index J(Nv[2]+1);
+    NDIndex<2> domain2d;
+    domain2d[0]=I;
+    domain2d[1]=J;
+    
+    Mesh2d_t mesh2d = Mesh2d_t(domain2d, spacings, origin);
+//     std::unique_ptr<FieldLayout2d_t> layout2d = std::unique_ptr<FieldLayout2d_t>( new FieldLayout2d_t(mesh2d) );
+    FieldLayout2d_t* layout2d = new FieldLayout2d_t(mesh2d);
+    
+    mesh2d.set_meshSpacing(&(spacings[0]));
+    mesh2d.set_origin(origin);
 
-        ipplProjection(nr,
-                       Vektor<double, 3>(lower[0], lower[1], lower[2]),
-                       Vektor<double, 3>(upper[0], upper[1], upper[2]),
-                       Vektor<std::size_t, 3>(4, 4, 32),
-                       Vektor<std::size_t, 3>(8, 8, 128),
-                       Vektor<double, 3>(6.0, 6.0, 6.0),
+    domain2d = layout2d->getDomain();
+    
+    BConds<double, 2, Mesh2d_t, Center_t> BC;
+    if (Ippl::getNodes()>1) {
+        BC[0] = new ParallelInterpolationFace<double,2,Mesh2d_t, Center_t>(0);
+        BC[1] = new ParallelInterpolationFace<double,2,Mesh2d_t, Center_t>(1);
+        BC[2] = new ParallelInterpolationFace<double,2,Mesh2d_t, Center_t>(2);
+        BC[3] = new ParallelInterpolationFace<double,2,Mesh2d_t, Center_t>(3);
+    }
+    else {
+        BC[0] = new InterpolationFace<double,2,Mesh2d_t, Center_t>(0);
+        BC[1] = new InterpolationFace<double,2,Mesh2d_t, Center_t>(1);
+        BC[2] = new InterpolationFace<double,2,Mesh2d_t, Center_t>(2);
+        BC[3] = new InterpolationFace<double,2,Mesh2d_t, Center_t>(3);
+    }
+    
+    NDIndex<2> lDom = domain2d;
+    Vektor<double,3> dx = (extend_r - extend_l) / Vector_t(Nx);
+    Vektor<double,3> dv = 2. * Vmax / Vector_t(Nv);
+    
+    
+    // field is used for twostream instability as 2D phase space mesh
+    Field2d_t field;
+//     field.initialize(mesh2d, *(layout2d.get()), GuardCellSizes<2>(1),BC);
+    field.initialize(mesh2d, *layout2d, GuardCellSizes<2>(2),BC);
+    
+    
+    
+    
+    // --------------------------------------------------------------------
+    
+    for (std::size_t i = 0; i < nIter; ++i) {
+        msg << "Processing step " << i << endl;
+
+        ipplProjection(field, dx, dv, Vmax, lDom,
                        bunch.get(), i);
         
         
@@ -402,6 +330,8 @@ void doTwoStream(Vektor<std::size_t, 3> nr,
         
         /* epsilon_0 not used by Ulmer --> multiply it away */
         assign(bunch->P, bunch->P + dt * bunch->qm / bunch->mass * bunch->E * Physics::epsilon_0);
+        
+        msg << "Done with step " << i << endl;
     }
 }
 
@@ -458,9 +388,9 @@ int main(int argc, char *argv[]) {
              << std::setfill('0') << std::setw(6) << Ippl::getNodes()
              << "-threads-1.dat";
     
-    if ( argc == 8 ) {
+    if ( argc == 9 ) {
         timefile.str(std::string());
-        timefile << std::string(argv[7]);
+        timefile << std::string(argv[8]);
     }
     
     IpplTimings::print(timefile.str());
