895ff97f827f22583b2d1cd1fe595e5734351468
Matthias Frey <matthias.frey@psi.ch>
2017-02-25 17:04:53 +0100

    AMR: Tagging test example

---
+++ ippl/test/AMR/ippl-amr/testTagging.cpp
@@ -0,0 +1,221 @@
+/*!
+ * @file testTagging.cpp
+ * @author Matthias Frey
+ * @date February 2017
+ * 
+ * Domain:  [-0.5, 0.5] x [-0.5, 0.5] x [-0.5, 0.5]\n
+ * BC:      Dirichlet boundary conditions\n
+ * Charge/particle: 1e-10\n
+ * Gaussian particle distribution N(0.0, 0.07)
+ * 
+ * @details Test the different tagging schemes.
+ * 
+ * Call:\n
+ *  mpirun -np [#cores] testTagging [#gridpoints x] [#gridpoints y] [#gridpoints z]
+ *                                     [#particles] [#levels] [max. box size]
+ * 
+ * @brief Perturbes particles randomly in space for several time steps.
+ */
+
+#include "Ippl.h"
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <iomanip>
+
+
+#include <ParmParse.H>
+
+
+#include "../Distribution.h"
+#include "../Solver.h"
+#include "../AmrOpal.h"
+
+#include "../helper_functions.h"
+
+// #include "writePlotFile.H"
+
+#include <cmath>
+
+#include "Physics/Physics.h"
+
+typedef AmrOpal::amrplayout_t amrplayout_t;
+typedef AmrOpal::amrbase_t amrbase_t;
+typedef AmrOpal::amrbunch_t amrbunch_t;
+
+typedef Vektor<double, BL_SPACEDIM> Vector_t;
+
+void doBoxLib(const Vektor<size_t, 3>& nr, size_t nParticles,
+              int nLevels, size_t maxBoxSize, int nSteps, Inform& msg)
+{
+    static IpplTimings::TimerRef tag1Timer = IpplTimings::getTimer("regrid-pot");
+    static IpplTimings::TimerRef tag2Timer = IpplTimings::getTimer("regrid-efield");
+    static IpplTimings::TimerRef tag3Timer = IpplTimings::getTimer("regrid-charge");
+    // ========================================================================
+    // 1. initialize physical domain (just single-level)
+    // ========================================================================
+    
+    std::array<double, BL_SPACEDIM> lower = {{-0.5, -0.5, -0.5}}; // m
+    std::array<double, BL_SPACEDIM> upper = {{ 0.5,  0.5,  0.5}}; // m
+    
+    RealBox domain;
+    
+    // in helper_functions.h
+    init(domain, nr, lower, upper);
+    
+    
+    /*
+     * create an Amr object
+     */
+    ParmParse pp("amr");
+    pp.add("max_grid_size", int(maxBoxSize));
+    
+    Array<int> error_buf(nLevels, 0);
+    
+    pp.addarr("n_error_buf", error_buf);
+    pp.add("grid_eff", 0.95);
+    
+    Array<int> nCells(3);
+    for (int i = 0; i < 3; ++i)
+        nCells[i] = nr[i];
+    
+    AmrOpal myAmrOpal(&domain, nLevels - 1, nCells, 0 /* cartesian */);
+    
+    
+    // ========================================================================
+    // 2. initialize all particles (just single-level)
+    // ========================================================================
+    
+    const Array<BoxArray>& ba = myAmrOpal.boxArray();
+    const Array<DistributionMapping>& dmap = myAmrOpal.DistributionMap();
+    const Array<Geometry>& geom = myAmrOpal.Geom();
+    
+    Array<int> rr(nLevels);
+    for (int i = 0; i < nLevels; ++i)
+        rr[i] = 2;
+    
+    
+    amrplayout_t* playout = new amrplayout_t(geom, dmap, ba, rr);
+    
+    std::unique_ptr<amrbunch_t> bunch( new amrbunch_t() );
+    bunch->initialize(playout);
+    bunch->initializeAmr(); // add attributes: level, grid
+    
+    bunch->setAllowParticlesNearBoundary(true);
+    
+    // initialize a particle distribution
+    unsigned long int nloc = nParticles / ParallelDescriptor::NProcs();
+    Distribution dist;
+    dist.gaussian(0.0, 0.07, nloc, ParallelDescriptor::MyProc());
+    
+    // copy particles to the PartBunchBase object.
+    dist.injectBeam( *(bunch.get()) );
+    
+    for (std::size_t i = 0; i < bunch->getLocalNum(); ++i) {
+        bunch->qm[i] = 1.0e-10;  // in [C]
+    }
+    
+    // redistribute on single-level
+    bunch->update();
+    
+    msg << "Single-level statistics" << endl;
+    bunch->gatherStatistics();
+    
+    msg << "#Particles: " << nParticles << endl
+        << "Charge per particle: " << bunch->qm[0] << " C" << endl
+        << "Total charge: " << nParticles * bunch->qm[0] << " C" << endl;
+    
+    
+    myAmrOpal.setBunch(bunch.get());
+    
+    const Array<Geometry>& geoms = myAmrOpal.Geom();
+    
+    // tagging using potential strength
+    myAmrOpal.setTagging(AmrOpal::kPotentialStrength);
+    
+    for (int i = 0; i <= myAmrOpal.finestLevel() && i < myAmrOpal.maxLevel(); ++i)
+        myAmrOpal.regrid(i /*lbase*/, 0.0 /*time*/);
+    
+    msg << "Distribution after tagging with potential:" << endl;
+    bunch->gatherStatistics();
+    bunch->python_format(0);
+    
+    // tagging using efield
+    myAmrOpal.setTagging(AmrOpal::kEfieldGradient);
+    
+    for (int i = 0; i <= myAmrOpal.finestLevel() && i < myAmrOpal.maxLevel(); ++i)
+        myAmrOpal.regrid(i /*lbase*/, 0.0 /*time*/);
+    
+    
+    msg << "Distribution after tagging with efield:" << endl;
+    bunch->gatherStatistics();
+    bunch->python_format(1);
+    
+    // tagging using charge
+    myAmrOpal.setTagging(AmrOpal::kChargeDensity);
+    
+    for (int i = 0; i <= myAmrOpal.finestLevel() && i < myAmrOpal.maxLevel(); ++i)
+        myAmrOpal.regrid(i /*lbase*/, 0.0 /*time*/);
+    
+    msg << "Distribution after tagging with charge:" << endl;
+    bunch->gatherStatistics();
+    bunch->python_format(2);
+}
+
+
+int main(int argc, char *argv[]) {
+    
+    Ippl ippl(argc, argv);
+    
+    Inform msg("Solver");
+    
+
+    static IpplTimings::TimerRef mainTimer = IpplTimings::getTimer("main");
+    IpplTimings::startTimer(mainTimer);
+
+    std::stringstream call;
+    call << "Call: mpirun -np [#procs] " << argv[0]
+         << " [#gridpoints x] [#gridpoints y] [#gridpoints z] [#particles] "
+         << "[#levels] [max. box size]";
+    
+    if ( argc < 7 ) {
+        msg << call.str() << endl;
+        return -1;
+    }
+    
+    // number of grid points in each direction
+    Vektor<size_t, 3> nr(std::atoi(argv[1]),
+                         std::atoi(argv[2]),
+                         std::atoi(argv[3]));
+    
+    
+    size_t nParticles = std::atoi(argv[4]);
+    
+    
+    msg << "Particle test running with" << endl
+        << "- #particles = " << nParticles << endl
+        << "- grid       = " << nr << endl;
+        
+    BoxLib::Initialize(argc,argv, false);
+    size_t nLevels = std::atoi(argv[5]) + 1; // i.e. nLevels = 0 --> only single level
+    size_t maxBoxSize = std::atoi(argv[6]);
+    int nSteps = std::atoi(argv[7]);
+    doBoxLib(nr, nParticles, nLevels, maxBoxSize, nSteps, msg);
+    
+    
+    IpplTimings::stopTimer(mainTimer);
+
+    IpplTimings::print();
+    
+    std::stringstream timefile;
+    timefile << std::string(argv[0]) << "-timing-cores-"
+             << std::setfill('0') << std::setw(6) << Ippl::getNodes()
+             << "-threads-1.dat";
+    
+    IpplTimings::print(timefile.str());
+    
+    return 0;
+}
