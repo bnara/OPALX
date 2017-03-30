89e6c7c837ce5b429dd43db1c38d08fd5d7fecc7
Matthias Frey <matthias.frey@psi.ch>
2017-03-30 16:53:59 +0200

    AMR: Clean up code

--- ippl/src/AmrParticle/ParticleAmrLayout.h
+++ ippl/src/AmrParticle/ParticleAmrLayout.h
@@ -2,9 +2,6 @@
 #define PARTICLE_AMR_LAYOUT_H
 
 #include "Particle/ParticleLayout.h"
-// #include "AmrParticle/AmrParticleBase.h"
-
-// #include <memory>
 
 template<class T, unsigned Dim>
 class ParticleAmrLayout : public ParticleLayout<T, Dim> {
@@ -20,25 +17,6 @@ public:
     // type of attributes this layout should use for position and ID
     typedef ParticleAttrib<SingleParticlePos_t> ParticlePos_t;
     typedef ParticleAttrib<Index_t>             ParticleIndex_t;
-    
-public:
-/*    
-    AmrLayout* getAmrLayout() {
-        return layout_mp->get();
-    }
-    
-    const AmrLayout* getAmrLayout() const {
-        return layout_mp->get();
-    }*/
-    
-//     void update(ParticleBase_t& PData, int minLevel = 0,
-//                 const ParticleAttrib<char> canSwap = 0)
-//     {
-// //         layout_mp->update(PData, minLevel, canSwap);
-//     }
-    
-// private:
-//     std::unique_ptr<AmrLayout> layout_mp;
 };
 
 #endif
