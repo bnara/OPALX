e6962b24a82c44a9084b91fdcf92351a13f61a80
Christof Kraus <christof.j.kraus@gmail.com>
2017-07-11 22:17:02 +0200

    fixing issue #125

--- src/Expressions/SRefExpr.h
+++ src/Expressions/SRefExpr.h
@@ -175,15 +175,8 @@ namespace Expressions {
             itsObject = OpalData::getInstance()->find(obj_name);
             if(itsObject == 0) {
                 if(att_name.empty()) {
-                    Object *variable = OpalData::getInstance()->find("REAL_VARIABLE");
-                    itsObject = variable->clone(obj_name);
-                    OpalData::getInstance()->define(itsObject);
-
-                    if(Options::verify) {
-                        std::cerr << "\nThe <variable> \"" << obj_name
-                                  << "\" is unknown, created with zero value.\n"
-                                  << std::endl;
-                    }
+                    throw OpalException("SRefExpr::fill()",
+                                        "\nThe <variable> \"" + obj_name + "\" is unknown.\n");
                 } else {
                     throw OpalException("SRefExpr::fill()",
                                         "Object \"" + obj_name + "\" is unknown.");
