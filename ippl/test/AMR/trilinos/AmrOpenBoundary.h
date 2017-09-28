4864cf01b3cdecbfef68ae4b3dbfa32025f397e7
Matthias Frey <matthias.frey@psi.ch>
2017-09-28 17:48:12 +0200

    AMR-Trilinos: Add files

---
+++ ippl/test/AMR/trilinos/AmrOpenBoundary.h
@@ -0,0 +1,60 @@
+#ifndef AMR_OPEN_BOUNDARY_H
+#define AMR_OPEN_BOUNDARY_H
+
+#include "AmrBoundary.h"
+
+template <class AmrMultiGridLevel>
+class AmrOpenBoundary : public AmrBoundary<AmrMultiGridLevel> {
+    
+public:
+    void apply(const AmrIntVect_t& iv,
+               typename AmrMultiGridLevel::indices_t& indices,
+               typename AmrMultiGridLevel::coefficients_t& values,
+               const double& value,
+               AmrMultiGridLevel* mglevel,
+               const int* nr);
+};
+
+
+template <class AmrMultiGridLevel>
+void AmrOpenBoundary<AmrMultiGridLevel>::apply(const AmrIntVect_t& iv,
+                                                    typename AmrMultiGridLevel::indices_t& indices,
+                                                    typename AmrMultiGridLevel::coefficients_t& values,
+                                                    const double& value,
+                                                    AmrMultiGridLevel* mglevel,
+                                                    const int* nr)
+{
+    /* there should be only one boundary at a time, i.e.
+     * either x-, y- or z-direction
+     */
+    
+    /* depending on boundary we need forward
+     * or backward difference for the gradient
+     */
+    
+    // find interior neighbour cells
+    AmrIntVect_t niv = iv;
+    AmrIntVect_t n2iv = iv; // next interior cell
+
+    for (int d = 0; d < BL_SPACEDIM; ++d) {
+        
+        if ( niv[d] == -1 ) {
+            // lower boundary --> forward difference
+            niv[d] = 0;
+            n2iv[d] = 1;
+            
+        } else if ( niv[d] == nr[d] ) {
+            // upper boundary --> backward difference
+            niv[d] = nr[d] - 1;
+            n2iv[d] = nr[d] - 2;
+        }
+    }
+    
+    indices.push_back( mglevel->serialize(niv) );
+    values.push_back( 2.0 * value );
+    
+    indices.push_back( mglevel->serialize(n2iv) );
+    values.push_back( - value );
+}
+
+#endif
