3ba0283178e8ad659f93b6dfb31ac33097abb8ec
Matthias Frey <matthias.frey@psi.ch>
2017-10-04 17:37:34 +0200

    AMR-Trilinos: Add Lagrange interpolater class

---
+++ ippl/test/AMR/trilinos/AmrLagrangeInterpolater.h
@@ -0,0 +1,111 @@
+#ifndef AMR_LAGRANGE_INTERPOLATER_H
+#define AMR_LAGRANGE_INTERPOLATER_H
+
+#include "AmrInterpolater.h"
+
+template <class AmrMultiGridLevel>
+class AmrLagrangeInterpolater : public AmrInterpolater<AmrMultiGridLevel>
+{
+public:
+    
+    enum Order {
+        LINEAR = 1,
+        QUADRATIC
+    };
+    
+    AmrLagrangeInterpolater(Order order);
+    
+    void stencil(const AmrIntVect_t& iv,
+                 typename AmrMultiGridLevel::indices_t& indices,
+                 typename AmrMultiGridLevel::coefficients_t& values,
+                 AmrMultiGridLevel* mglevel);
+    
+    void coarse(const AmrIntVect_t& iv,
+                typename AmrMultiGridLevel::indices_t& indices,
+                typename AmrMultiGridLevel::coefficients_t& values,
+                int direction, int shift, AmrMultiGridLevel* mglevel);
+    
+    void fine(const AmrIntVect_t& iv,
+              typename AmrMultiGridLevel::indices_t& indices,
+              typename AmrMultiGridLevel::coefficients_t& values,
+              int direction, int shift, AmrMultiGridLevel* mglevel);
+    
+private:
+    
+    /*!
+     * First order interpolation on fine cell interface side
+     * 
+     * @param iv is the fine ghost cell at the interface (on coarse cell that is not
+     * refined)
+     * @param indices global matrix indices of fine level cells
+     * @param values matrix entries of fine level cells (coefficients)
+     * @param direction of interface (0 "horizontal", 1 "vertical", 2 "longitudinal")
+     * @param shift is either -1 or 1. If the refined coarse cell is on the left / lower / front
+     * side, shift is equal to -1, otherwise the interface is on the right / upper / back side
+     * and the value is 1.
+     * @param mglevel used to get the global indices and refinement ratio among levels,
+     * and boundary avlues at physical domain, e.g. Dirichlet, open BC
+     */
+    void fineLinear_m(const AmrIntVect_t& iv,
+                  typename AmrMultiGridLevel::indices_t& indices,
+                  typename AmrMultiGridLevel::coefficients_t& values,
+                  int direction, int shift, AmrMultiGridLevel* mglevel);
+    
+    /*!
+     * Second order interpolation on fine cell interface side
+     * 
+     * @param iv is the fine ghost cell at the interface (on coarse cell that is not
+     * refined)
+     * @param indices global matrix indices of fine level cells
+     * @param values matrix entries of fine level cells (coefficients)
+     * @param direction of interface (0 "horizontal", 1 "vertical", 2 "longitudinal")
+     * @param shift is either -1 or 1. If the refined coarse cell is on the left / lower / front
+     * side, shift is equal to -1, otherwise the interface is on the right / upper / back side
+     * and the value is 1.
+     * @param mglevel used to get the global indices and refinement ratio among levels,
+     * and boundary avlues at physical domain, e.g. Dirichlet, open BC
+     */
+    void fineQuadratic_m(const AmrIntVect_t& iv,
+                     typename AmrMultiGridLevel::indices_t& indices,
+                     typename AmrMultiGridLevel::coefficients_t& values,
+                     int direction, int shift, AmrMultiGridLevel* mglevel);
+    
+    /*
+     * First oder interpolation on coarse cell interface side
+     * @param iv is the coarse cell at the interface (center cell of Laplacian)
+     * @param indices global matrix indices of coarse level cells
+     * @param values matrix entries of coarse level cells (coefficients)
+     * @param direction of interface (0 "horizontal", 1 "vertical", 2 "longitudinal")
+     * @param shift is either -1 or 1. If the refined coarse cell is on the left / lower / front
+     * side, shift is equal to -1, otherwise the interface is on the right / upper / back side
+     * and the value is 1.
+     * @param mglevel used to get the global indices and refinement ratio among levels,
+     * and boundary values at physical domain, e.g. Dirichlet, open BC
+     */
+    void crseLinear_m(const AmrIntVect_t& iv,
+                      typename AmrMultiGridLevel::indices_t& indices,
+                      typename AmrMultiGridLevel::coefficients_t& values,
+                      int direction, int shift, AmrMultiGridLevel* mglevel);
+    
+    /*!
+     * Second order interpolation on coarse cell interface side
+     * 
+     * @param iv is the coarse cell at the interface (center cell of Laplacian)
+     * @param indices global matrix indices of coarse level cells
+     * @param values matrix entries of coarse level cells (coefficients)
+     * @param direction of interface (0 "horizontal", 1 "vertical", 2 "longitudinal")
+     * @param shift is either -1 or 1. If the refined coarse cell is on the left / lower / front
+     * side, shift is equal to -1, otherwise the interface is on the right / upper / back side
+     * and the value is 1.
+     * @param mglevel used to get the global indices and refinement ratio among levels,
+     * and boundary values at physical domain, e.g. Dirichlet, open BC
+     */
+    void crseQuadratic_m(const AmrIntVect_t& iv,
+                         typename AmrMultiGridLevel::indices_t& indices,
+                         typename AmrMultiGridLevel::coefficients_t& values,
+                         int direction, int shift, AmrMultiGridLevel* mglevel);
+};
+
+#include "AmrLagrangeInterpolater.hpp"
+
+#endif
---
+++ ippl/test/AMR/trilinos/AmrLagrangeInterpolater.hpp
@@ -0,0 +1,246 @@
+template <class AmrMultiGridLevel>
+AmrLagrangeInterpolater<AmrMultiGridLevel>::AmrLagrangeInterpolater(Order order)
+    : AmrInterpolater( int(order) + 1 )
+{ }
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::stencil(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    AmrMultiGridLevel* mglevel)
+{
+    
+}
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::coarse(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    int direction, int shift, AmrMultiGridLevel* mglevel)
+{
+    // polynomial degree = #points - 1
+    switch ( nPoints_m - 1 ) {
+        
+        case Order::LINEAR:
+            this->crseLinear_m(iv, indices, values, direction, shift, mglevel);
+            break;
+        case Order::QUADRATIC:
+            this->crseQuadratic_m(iv, indices, values, direction, shift, mglevel);
+            break;
+        default:
+            std::runtime_error("Not implemented interpolation");
+    }
+}
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::fine(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    int direction, int shift, AmrMultiGridLevel* mglevel)
+{
+    // polynomial degree = #points - 1
+    switch ( nPoints_m - 1 ) {
+        
+        case Order::LINEAR:
+            this->fineLinear_m(iv, indices, values, direction, shift, mglevel);
+            break;
+        case Order::QUADRATIC:
+            this->fineQuadratic_m(iv, indices, values, direction, shift, mglevel);
+            break;
+        default:
+            std::runtime_error("Not implemented interpolation");
+    }
+}
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::fineLinear_m(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    int direction, int shift, AmrMultiGridLevel* mglevel)
+{
+    // not implemented
+    std::runtime_error("Not implemented interpolation");
+}
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::fineQuadratic_m(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    int direction, int shift, AmrMultiGridLevel* mglevel)
+{
+    // first fine cell on refined coarse cell (closer to interface)
+    iv[d] += shift;
+    indices.push_back( mglevel->serialize(iv) );
+    values.push_back( 2.0 / 3.0 );
+                        
+    // second fine cell on refined coarse cell (further away from interface)
+    iv[d] += shift;
+    indices.push_back( mglevel->serialize(iv) );
+    values.push_back( -0.2 );
+}
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::crseLinear_m(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    int direction, int shift, AmrMultiGridLevel* mglevel)
+{
+    //TODO Extend to 3D
+    
+    // y_t + y_b
+    indices.push_back( mglevel->serialize(iv) );
+    values.push_back( shift * 2.0 );
+    
+    // the neighbour cancels out
+}
+
+
+template <class AmrMultiGridLevel>
+void AmrLagrangeInterpolater<AmrMultiGridLevel>::crseQuadratic_m(
+    const AmrIntVect_t& iv,
+    typename AmrMultiGridLevel::indices_t& indices,
+    typename AmrMultiGridLevel::coefficients_t& values,
+    int direction, int shift, AmrMultiGridLevel* mglevel)
+{
+    //TODO Extend to 3D
+    
+    // right / upper / back
+    IntVect niv = iv;
+    niv[(d+1)%BL_SPACEDIM ] += 1;
+    
+    // left / lower / front
+    IntVect miv = iv;
+    miv[(d+1)%BL_SPACEDIM ] -= 1;
+    
+    // 2nd right / upper / back
+    IntVect n2iv = niv;
+    n2iv[(d+1)%BL_SPACEDIM ] += 1;
+    
+    // 2nd left / lower / front
+    IntVect m2iv = miv;
+    m2iv[(d+1)%BL_SPACEDIM ] -= 1;
+        
+    /* 3 cases:
+     * --------
+     * r: right neighbour of iv (center of Laplacian)
+     * u: upper neighbour of iv
+     * b: back neighbour of iv
+     * 
+     * l: lower / left neighbour of iv
+     * f: front neighbour of iv
+     * 
+     * -1 --> not valid, error happend
+     * 0 --> r / u / b and 2nd r / u / b are valid
+     * 1 --> direct neighbours (r / u / b and l / f) of iv are valid (has priority)
+     * 2 --> l / f and 2nd l / f are valid
+     */
+    
+    // check r / u / b --> 1: valid; 0: not valid
+    bool rub = !crse_fine_ba.contains(niv);
+    
+    // check l / f --> 1: valid; 0: not valid
+    bool lf = !crse_fine_ba.contains(miv);
+    
+    // check 2nd r / u / b
+    bool rub2 = !crse_fine_ba.contains(n2iv);
+    
+    // check 2nd l / f
+    bool lf2 = !crse_fine_ba.contains(m2iv);
+    
+    if ( rub && lf )
+    {
+        /*
+         * standard case -1, +1 are not-refined nor at physical/mesh boundary
+         */            
+        // cell is not refined and not at physical boundary
+        
+        // y_t + y_b
+        indices.push_back( mglevel->serialize(iv) );
+        values.push_back( 2.0 * shift * 0.5 );
+        
+        //                      y_t          y_b
+        double value = shift * (1.0 / 12.0 - 0.05);
+        if ( mglevel->isBoundary(niv) ) {
+            mglevel->applyBoundary(niv, indices, values, value);
+        } else {
+            indices.push_back( mglevel->serialize(niv) );
+            values.push_back( value );
+        }
+        
+        //               y_t     y_b
+        value = shift * (-0.05 + 1.0 / 12.0);
+        if ( mglevel->isBoundary(miv) ) {
+            mglevel->applyBoundary(miv, indices, values, value);
+        } else {
+            indices.push_back( mglevel->serialize(miv) );
+            values.push_back( value );
+        }
+    } else if ( rub && rub2 ) {
+        /*
+         * corner case --> right / upper / back + 2nd right / upper / back
+         */
+        // y_t + y_b
+        indices.push_back( mglevel->serialize(iv) );
+        values.push_back( shift * (7.0 / 12.0 + 0.75) );
+        
+        double value = - shift / 15.0;
+        if ( mglevel->isBoundary(niv) ) {
+            mglevel->applyBoundary(niv, indices, values, value);
+        } else {
+            indices.push_back( mglevel->serialize(niv) );
+            values.push_back( value );
+        }
+        
+        value = shift * ( 1.0 / 12.0 - 0.05 );
+        if ( mglevel->isBoundary(n2iv) ) {
+            mglevel->applyBoundary(n2iv, indices, values, value);
+        } else {
+            indices.push_back( mglevel->serialize(n2iv) );
+            values.push_back( value );
+        }
+        
+    } else if ( lf && lf2 ) {
+        /*
+         * corner case --> left / lower / front + 2nd left / lower / front
+         */
+        // y_t + y_b
+        indices.push_back( mglevel->serialize(iv) );
+        values.push_back( shift * (7.0 / 12.0 + 0.75) );
+        
+        double value = - shift / 15.0;
+        if ( mglevel->isBoundary(miv) ) {
+            mglevel->applyBoundary(miv, indices, values, value);
+        } else {
+            indices.push_back( mglevel->serialize(miv) );
+            values.push_back( value );
+        }
+        
+        value = shift * ( 1.0 / 12.0 - 0.05 );
+        if ( mglevel->isBoundary(m2iv) ) {
+            mglevel->applyBoundary(m2iv, indices, values, value);
+        } else {
+            indices.push_back( mglevel->serialize(m2iv) );
+            values.push_back( value );
+        }
+        
+    } else {
+        // last trial: linear Lagrange interpolation
+        
+        if ( rub || lf ) {
+            this->crseLinear_m(iv, indices, values, direction, shift, mglevel);
+        } else
+            std::runtime_error("Lagrange Error: No valid scenario found!");
+    }
+}
