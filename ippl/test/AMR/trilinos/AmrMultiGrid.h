71f64b0983db518c220bab34f177e4adbf93faff
Matthias Frey <matthias.frey@psi.ch>
2017-10-17 09:52:02 +0200

    AMR-Trilinos: Fix Teuchos object deletion issue.
    modified:   AmrMultiGrid.h
    modified:   BlockCGSolMgr.h

--- ippl/test/AMR/trilinos/AmrMultiGrid.h
+++ ippl/test/AMR/trilinos/AmrMultiGrid.h
@@ -214,7 +214,7 @@ private:
     
     std::vector<std::unique_ptr<AmrMultiGridLevel_t > > mglevel_m;
     
-    std::unique_ptr<LinearSolver<Teuchos::RCP<matrix_t>, Teuchos::RCP<vector_t> > > solver_mp;
+    std::shared_ptr<LinearSolver<Teuchos::RCP<matrix_t>, Teuchos::RCP<vector_t> > > solver_mp;
 //     TrilinosSolver solver_m;
     
     AmrIntVect_t rr_m;      //TODO move to level
--- ippl/test/AMR/trilinos/BlockCGSolMgr.h
+++ ippl/test/AMR/trilinos/BlockCGSolMgr.h
@@ -19,13 +19,26 @@ public:
     typedef amr::matrix_t matrix_t;
     typedef amr::vector_t vector_t;
     
+    typedef Belos::BlockCGSolMgr<double, Epetra_MultiVector, Epetra_Operator> solver_t;
+    typedef Belos::LinearProblem<double, Epetra_MultiVector, Epetra_Operator> problem_t;
+    
 public:
     
-    BlockCGSolMgr() : problem(Teuchos::null), params(Teuchos::null) { }
+    BlockCGSolMgr() : problem_mp(Teuchos::null) {
+        params_mp = Teuchos::rcp( new Teuchos::ParameterList );
+    //     params->set( "Block Size", 4 );
+        params_mp->set("Maximum Iterations", 1000);
+        params_mp->set("Convergence Tolerance", 1.0e-9);
+        params_mp->set("Block Size", 32);
+        
+        solver_mp = Teuchos::rcp( new solver_t() );
+        solver_mp->setParameters(params_mp);
+    }
     
     ~BlockCGSolMgr() {
-        problem = Teuchos::null;
-        params = Teuchos::null;
+        problem_mp = Teuchos::null;
+        params_mp = Teuchos::null;
+        solver_mp = Teuchos::null;
     };
     
     void solve(const Teuchos::RCP<matrix_t>& A,
@@ -35,50 +48,47 @@ public:
         /*
          * solve linear system Ax = b
          */
-        problem = Teuchos::rcp( new Belos::LinearProblem<double, Epetra_MultiVector, Epetra_Operator>(A, x, b) );
+        problem_mp = Teuchos::rcp( new problem_t(A, x, b) );
         
         
-        if ( !problem->setProblem() )
+        if ( !problem_mp->setProblem() )
             throw std::runtime_error("Belos::LinearProblem failed to set up correctly!");
         
-        params = Teuchos::rcp( new Teuchos::ParameterList );
+        solver_mp->setProblem(problem_mp);
         
-    //     params->set( "Block Size", 4 );
-        params->set("Maximum Iterations", 1000);
-        params->set("Convergence Tolerance", 1.0e-9);
-        params->set("Block Size", 32);
-        
-        Belos::BlockCGSolMgr<double, Epetra_MultiVector, Epetra_Operator> solver(problem, params);
-        
-        Belos::ReturnType ret = solver.solve();
+        Belos::ReturnType ret = solver_mp->solve();
         
         // get the solution from the problem
         if ( ret == Belos::Converged ) {
             x = Teuchos::null;
-            x = Teuchos::rcp( new vector_t(Epetra_DataAccess::Copy, *problem->getLHS(), 0) );
+            x = Teuchos::rcp( new vector_t(Epetra_DataAccess::Copy, *problem_mp->getLHS(), 0) );
             
             // print stuff
 //             if ( epetra_comm_m.MyPID() == 0 ) {
-                std::cout << "Achieved tolerance: " << solver.achievedTol() << std::endl
-                        << "Number of iterations: " << solver.getNumIters() << std::endl;
+                std::cout << "Achieved tolerance: " << solver_mp->achievedTol() << std::endl
+                        << "Number of iterations: " << solver_mp->getNumIters() << std::endl;
 //             }
             
         } else {
 //             if ( epetra_comm_m.MyPID() == 0 ) {
-                std::cout << "Not converged. Achieved tolerance after " << solver.getNumIters() << " iterations is "
-                        << solver.achievedTol() << "." << std::endl;
+                std::cout << "Not converged. Achieved tolerance after " << solver_mp->getNumIters() << " iterations is "
+                        << solver_mp->achievedTol() << "." << std::endl;
 //             }
         }
+        
+        
+        problem_mp = Teuchos::null;
     }
     
     
     void residual(Teuchos::RCP<vector_t>& r, const Teuchos::RCP<vector_t>& x, const Teuchos::RCP<vector_t>& b) {
-        problem->computeCurrResVec(r.get(), x.get(), b.get());
+        problem_mp->computeCurrResVec(r.get(), x.get(), b.get());
     }
     
 private:
-    Teuchos::RCP<Belos::LinearProblem<double, Epetra_MultiVector, Epetra_Operator> > problem;
-    Teuchos::RCP<Teuchos::ParameterList> params;
+    Teuchos::RCP<problem_t> problem_mp;
+    Teuchos::RCP<Teuchos::ParameterList> params_mp;
+    Teuchos::RCP<solver_t>  solver_mp;
 };
 
 #endif
