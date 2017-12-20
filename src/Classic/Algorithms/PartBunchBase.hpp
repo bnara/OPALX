c31300840eb7cab3cdb6c5acfbeb6fa88ec51c81
Matthias Frey <matthias.frey@psi.ch>
2017-12-20 15:07:43 +0100

    Use getLocalBounds again

--- src/Classic/Algorithms/PartBunchBase.hpp
+++ src/Classic/Algorithms/PartBunchBase.hpp
@@ -1209,23 +1209,7 @@ void PartBunchBase<T, Dim>::setZ(int i, double zcoo)
 template <class T, unsigned Dim>
 void PartBunchBase<T, Dim>::get_bounds(Vector_t &rmin, Vector_t &rmax) {
     
-//    this->getLocalBounds(rmin, rmax);
-
-    const size_t localNum = getLocalNum();
-    
-    if ( localNum > 0 ) {
-	rmin = R[0];
-	rmax = R[0];
-	for (size_t i = 1; i < localNum; ++ i) {
-	    for (unsigned short d = 0; d < 3u; ++ d) {
-		if (rmin(d) > R[i](d)) rmin(d) = R[i](d);
-		if (rmax(d) < R[i](d)) rmax(d) = R[i](d);
-	    }
-	}
-    } else {
-	rmin = Vector_t(0.0, 0.0, 0.0);
-	rmax = Vector_t(0.0, 0.0, 0.0);
-    }
+    this->getLocalBounds(rmin, rmax);
     
     double min[Dim];
     double max[Dim];
@@ -1243,25 +1227,24 @@ void PartBunchBase<T, Dim>::get_bounds(Vector_t &rmin, Vector_t &rmax) {
         rmin[i] = min[i];
         rmax[i] = max[i];
     }
-    
-    //bounds(this->R, rmin, rmax);
-
-//    std::cout << rmin << std::endl;
-//    std::cout << rmax << std::endl;
 }
 
 
 template <class T, unsigned Dim>
 void PartBunchBase<T, Dim>::getLocalBounds(Vector_t &rmin, Vector_t &rmax) {
     const size_t localNum = getLocalNum();
-    if (localNum == 0) return;
-
+    if (localNum == 0) {
+	rmin = Vector_t(0.0, 0.0, 0.0);
+	rmax = Vector_t(0.0, 0.0, 0.0);
+	return;
+    }
+    
     rmin = R[0];
     rmax = R[0];
     for (size_t i = 1; i < localNum; ++ i) {
         for (unsigned short d = 0; d < 3u; ++ d) {
             if (rmin(d) > R[i](d)) rmin(d) = R[i](d);
-	    else if (rmax(d) < R[i](2)) rmax(d) = R[i](d);
+	    if (rmax(d) < R[i](d)) rmax(d) = R[i](d);
         }
     }
 }
