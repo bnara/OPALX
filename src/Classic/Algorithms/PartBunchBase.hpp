83636c2b8b8d74aa1818e3919fa3183ac606690a
Matthias Frey <matthias.frey@psi.ch>
2017-12-20 12:52:35 +0100

    Undo

--- src/Classic/Algorithms/PartBunchBase.hpp
+++ src/Classic/Algorithms/PartBunchBase.hpp
@@ -1209,7 +1209,20 @@ void PartBunchBase<T, Dim>::setZ(int i, double zcoo)
 template <class T, unsigned Dim>
 void PartBunchBase<T, Dim>::get_bounds(Vector_t &rmin, Vector_t &rmax) {
     
-    this->getLocalBounds(rmin, rmax);
+//    this->getLocalBounds(rmin, rmax);
+
+    const size_t localNum = getLocalNum();
+    
+    if ( localNum > 0 ) {
+	rmin = R[0];
+	rmax = R[0];
+	for (size_t i = 1; i < localNum; ++ i) {
+	    for (unsigned short d = 0; d < 3u; ++ d) {
+		if (rmin(d) > R[i](d)) rmin(d) = R[i](d);
+		if (rmax(d) < R[i](d)) rmax(d) = R[i](d);
+	    }
+	}
+    }
     
     double min[Dim];
     double max[Dim];
@@ -1240,7 +1253,7 @@ void PartBunchBase<T, Dim>::getLocalBounds(Vector_t &rmin, Vector_t &rmax) {
     for (size_t i = 1; i < localNum; ++ i) {
         for (unsigned short d = 0; d < 3u; ++ d) {
             if (rmin(d) > R[i](d)) rmin(d) = R[i](d);
-            if (rmax(d) < R[i](d)) rmax(d) = R[i](d);
+	    else if (rmax(d) < R[i](2)) rmax(d) = R[i](d);
         }
     }
 }
