5a428723956b106ce88d4f4f68990184515ecb06
Matthias Frey <matthias.frey@psi.ch>
2017-12-21 11:30:23 +0100

    Fix getLocalBounds()

--- src/Classic/Algorithms/PartBunchBase.hpp
+++ src/Classic/Algorithms/PartBunchBase.hpp
@@ -1244,7 +1244,7 @@ void PartBunchBase<T, Dim>::getLocalBounds(Vector_t &rmin, Vector_t &rmax) {
     for (size_t i = 1; i < localNum; ++ i) {
         for (unsigned short d = 0; d < 3u; ++ d) {
             if (rmin(d) > R[i](d)) rmin(d) = R[i](d);
-	    if (rmax(d) < R[i](d)) rmax(d) = R[i](d);
+	    else if (rmax(d) < R[i](d)) rmax(d) = R[i](d);
         }
     }
 }
