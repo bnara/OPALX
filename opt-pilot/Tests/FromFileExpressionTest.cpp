cac2c73e291bf329f309a54bbce8b9edadeb4e01
Christof Metzger-Kraus <christof.j.kraus@gmail.com>
2018-01-13 09:55:04 +0100

    adding tests with nested functions

--- opt-pilot/Tests/FromFileExpressionTest.cpp
+++ opt-pilot/Tests/FromFileExpressionTest.cpp
@@ -16,6 +16,26 @@
 
 namespace {
 
+struct my_sqrt {
+
+    Expressions::Result_t operator()(client::function::arguments_t args) {
+
+        double value = boost::get<double>(args[0]);
+        if (value < 0.0) return boost::make_tuple(0.0, false);
+        return boost::make_tuple(sqrt(value), true);
+    }
+};
+
+struct my_pow {
+
+    Expressions::Result_t operator()(client::function::arguments_t args) {
+
+        double base = boost::get<double>(args[0]);
+        double exponent = boost::get<double>(args[1]);
+        return boost::make_tuple(pow(base, exponent), true);
+    }
+};
+
     // The fixture for testing class Foo.
     class FromFileExpressionTest : public ::testing::Test {
     protected:
@@ -54,7 +74,38 @@ namespace {
         funcs.insert(std::pair<std::string, client::function::type>
                 ("fromFile", ff));
 
-        std::string testexpr = "fromFile(\"resources/fromfile_test.dat\", \"var\")";
+        std::string testexpr = "fromFile(\"resources/fromfile_test.dat\")";
+        boost::scoped_ptr<Expression> e(new Expression(testexpr, funcs));
+        Expressions::Result_t result;
+        EXPECT_NO_THROW({
+            result = e->evaluate(vars);
+        });
+
+        ASSERT_NEAR(expected, boost::get<0>(result), 1e-6);
+        ASSERT_TRUE(boost::get<1>(result));
+    }
+
+    TEST_F(FromFileExpressionTest, EvaluateCombinedFromFileExpression) {
+
+        variableDictionary_t vars;
+        double expected = sqrt(pow(0.3126 + 0.3561 + 0.4242, 2) + pow(0.1263 - 0.5613 + 0.2424, 2));
+
+        functionDictionary_t funcs;
+        client::function::type ff;
+        ff = FromFile();
+        funcs.insert(std::pair<std::string, client::function::type>
+                ("fromFile", ff));
+        client::function::type sqrt_func;
+        sqrt_func = my_sqrt();
+        funcs.insert(std::pair<std::string, client::function::type>
+                ("sqrt", sqrt_func));
+        client::function::type pow_func;
+        pow_func = my_pow();
+        funcs.insert(std::pair<std::string, client::function::type>
+                ("pow", pow_func));
+
+        std::string testexpr = "sqrt(pow(fromFile(\"resources/fromfile_test.dat\"), 2) + "
+            "pow(fromFile(\"resources/fromfile_test2.dat\"), 2))";
         boost::scoped_ptr<Expression> e(new Expression(testexpr, funcs));
         Expressions::Result_t result;
         EXPECT_NO_THROW({
