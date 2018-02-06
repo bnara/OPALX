56530b2d78f38c486f02b371876b2aac716983ac
Matthias Frey <matthias.frey@psi.ch>
2018-02-06 09:58:02 +0100

    DataSink:
    - remove replaceVersionString for lBal, grid and memory file
    - beautify file opening a bit

--- src/Structure/DataSink.h
+++ src/Structure/DataSink.h
@@ -247,13 +247,16 @@ private:
 
     void rewindLines(const std::string &fileName, size_t numberOfLines) const;
     void replaceVersionString(const std::string &fileName) const;
+    
+    void open_m(std::ofstream& os, const std::string& fileName) const;
+    
     /** \brief First write to the statistics output file.
      *
-     * Initially set to true so that SDDS format header information is written to file
+     * Initially set to std::ios::out so that SDDS format header information is written to file
      * during the first write call to the statistics output file. Variable is then
-     * reset to false so that header information is only written once.
+     * reset to std::ios::app so that header information is only written once.
      */
-    bool firstWriteToStat_m;
+    std::ios_base::openmode mode_m;
 
     /** \brief First write to the H5 surface loss file.
      *
