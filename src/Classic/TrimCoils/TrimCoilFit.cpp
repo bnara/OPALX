59f7d604b7e1f25a1ed485b74d64e5850b5ffeb9
Jochem Snuverink <jochem.snuverink@psi.ch>
2018-04-12 12:03:16 +0200

    start of rational function implementation

--- src/Classic/TrimCoils/TrimCoilFit.cpp
+++ src/Classic/TrimCoils/TrimCoilFit.cpp
@@ -1,5 +1,7 @@
 #include "TrimCoils/TrimCoilFit.h"
 
+#include <cmath>
+
 TrimCoilFit::TrimCoilFit(double bmax,
                          double rmin,
                          double rmax,
@@ -10,13 +12,31 @@ TrimCoilFit::TrimCoilFit(double bmax,
     coefnum_m(coefnum),
     coefdenom_m(coefdenom)
 {
-  // convert to m
-  const double mm2m = 0.001;
-  rmin_m = rmin * mm2m;
-  rmax_m = rmax * mm2m;
+    // convert to m
+    const double mm2m = 0.001;
+    rmin_m = rmin * mm2m;
+    rmax_m = rmax * mm2m;
 }
 
 void TrimCoilFit::doApplyField(const double r, const double z, double *br, double *bz)
 {
-  // FIXME
+    // FIXME
+
+    if (std::abs(bmax_m) < 1e-20) return;
+    // check range
+    if (r < rmin_m || r > rmax_m) return;
+
+    double num = 0.0;
+    for (std::size_t i = 0; i < coefnum_m.size(); ++i) {
+        num += std::pow(coefnum_m[i], i);
+    }
+    double denom = 0.0;
+    for (std::size_t i = 0; i < coefdenom_m.size(); ++i) {
+        denom += std::pow(coefdenom_m[i], i);
+    }
+
+    double dr = num / denom;
+
+    *bz -= 0.0;
+    *br -= dr*z;
 }
