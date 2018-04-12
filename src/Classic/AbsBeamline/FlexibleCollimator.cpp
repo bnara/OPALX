0666085e88d3d425d614803bf91b11fca1be319b
Christof Metzger-Kraus <christof.j.kraus@gmail.com>
2018-04-12 03:18:59 +0200

    only node 0 should write quadtree file

--- src/Classic/AbsBeamline/FlexibleCollimator.cpp
+++ src/Classic/AbsBeamline/FlexibleCollimator.cpp
@@ -242,6 +242,8 @@ void FlexibleCollimator::setDescription(const std::string &desc) {
     tree_m.objects_m.insert(tree_m.objects_m.end(), holes_m.begin(), holes_m.end());
     tree_m.buildUp();
 
-    std::ofstream out("data/quadtree.gpl");
-    tree_m.writeGnuplot(out);
+    if (Ippl::myNode() == 0) {
+        std::ofstream out("data/quadtree.gpl");
+        tree_m.writeGnuplot(out);
+    }
 }
