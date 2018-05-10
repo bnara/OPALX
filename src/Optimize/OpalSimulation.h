aa1935884ed16581d7f926bf42bd3f03bbd25dc6
Matthias Frey <matthias.frey@psi.ch>
2018-05-10 17:26:00 +0200

    fix reorder warning

--- src/Optimize/OpalSimulation.h
+++ src/Optimize/OpalSimulation.h
@@ -58,12 +58,11 @@ public:
     /// returns container containing all requested variables with results
     reqVarContainer_t getResults() { return requestedVars_; }
     
+    /// set job id (SAMPLE command)
     void setFilename(int id) { id_m = id; }
 
 private:
     
-    int id_m;
-
     /// identification of the simulation (corresponding to output filename)
     std::string simulationName_;
     /// full path of simulation directory (where simulation will be run)
@@ -92,7 +91,9 @@ private:
 
     /// object to generate simulation input files
     boost::scoped_ptr<GenerateOpalSimulation> gs_;
-
+    
+    /// job id (SAMPLE command)
+    int id_m;
 
     /// mark a solution as invalid
     void invalidBunch();
