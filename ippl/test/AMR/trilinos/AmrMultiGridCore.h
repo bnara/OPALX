869ff48c19604c567e62aab5b3976a95a0ca19f0
Matthias Frey <matthias.frey@psi.ch>
2018-05-31 15:13:46 +0200

    do not use FFT

--- ippl/test/AMR/trilinos/AmrMultiGridCore.h
+++ ippl/test/AMR/trilinos/AmrMultiGridCore.h
@@ -16,7 +16,7 @@
 #include "BelosBottomSolver.h"
 #include "Amesos2BottomSolver.h"
 #include "MueLuBottomSolver.h"
-#include "FFTBottomSolver.h"
+// #include "FFTBottomSolver.h"
 
 #include "AmrSmoother.h"
 
