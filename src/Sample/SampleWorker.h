43dda76b3afe3ad6677f77bfb61dc645aa5a92b7
Matthias Frey <matthias.frey@psi.ch>
2018-12-05 17:44:48 +0100

    Sampler: The base class of SamplePilot (i.e. Pilot.h) requires at least 1 objective. The SamplePilot adds a dummy objective.

--- src/Sample/SampleWorker.h
+++ src/Sample/SampleWorker.h
@@ -157,7 +157,8 @@ protected:
                 sim->collectResults();
                 requested_results = sim->getResults();
 
-                if ( !this->objectives_.empty() && filesToKeep_m.empty() ) {
+                // base clase of SamplePilot requires at least 1 objective --> dummy objective (SamplePilot, line 72)
+                if ( !(this->objectives_.size() - 1) && filesToKeep_m.empty() ) {
                     sim->cleanUp();
                 } else {
                     // if empty, we keep all files
