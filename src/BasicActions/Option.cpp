31ecb63650ab371e1de591402a1f3e9df5095784
Matthias Frey <matthias.frey@psi.ch>
2019-11-12 15:16:44 +0100

    extend description

--- src/BasicActions/Option.cpp
+++ src/BasicActions/Option.cpp
@@ -272,7 +272,7 @@ Option::Option():
         ("HALOSHIFT", "Constant parameter to shift halo value (default: 0.0)", haloShift);
 
     itsAttr[DELPARTFREQ] = Attributes::makeReal
-        ("DELPARTFREQ", "The frequency to delete particles: Default: 1", delPartFreq);
+        ("DELPARTFREQ", "The frequency to delete particles, i.e. delete when step%delPartFreq == 0. Default: 1", delPartFreq);
 
     registerOwnership(AttributeHandler::STATEMENT);
 
