8a73e359835b844e2b5c686ea8bc41c7ca4fe251
Achim Gsell <achim.gsell@psi.ch>
2020-01-09 14:10:11 +0100

    cleanup: __MWERKS__ accidental removed line re-added

--- ippl/src/Meshes/CartesianCentering.h
+++ ippl/src/Meshes/CartesianCentering.h
@@ -42,6 +42,7 @@ void CartesianCentering<CE,Dim,NComponents>::
 print_Centerings(std::ostream& out)
 {
   int i,j;
+  out << CenteringName << std::endl;
   out << "Dim = " << Dim << " ; NComponents = " << NComponents << std::endl;
   for (i=0;i<Dim;i++) {
     for (j=0;j<NComponents;j++) {
