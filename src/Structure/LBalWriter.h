e0568cd633533e37793a655baa6588838d87c374
Matthias Frey <matthias.frey@psi.ch>
2020-04-04 11:07:16 +0200

    add file header to writer classes

--- src/Structure/LBalWriter.h
+++ src/Structure/LBalWriter.h
@@ -1,3 +1,21 @@
+//
+// Class LBalWriter
+//   This class writes a SDDS file with MPI load balancing information.
+//
+// Copyright (c) 2019, Matthias Frey, Paul Scherrer Institut, Villigen PSI, Switzerland
+//                     Christof Metzger-Kraus, Open Sourcerer
+// All rights reserved
+//
+// This file is part of OPAL.
+//
+// OPAL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with OPAL. If not, see <https://www.gnu.org/licenses/>.
+//
 #ifndef OPAL_LBAL_WRITER_H
 #define OPAL_LBAL_WRITER_H
 
