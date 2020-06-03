44c6b3f35ca3d70f87a0e0edffa1cd708326a4ae
Matthias Frey <matthias.frey@psi.ch>
2020-06-03 13:18:55 +0200

    FieldWriter: add missing file header to *.hpp file

--- src/Structure/FieldWriter.hpp
+++ src/Structure/FieldWriter.hpp
@@ -1,3 +1,21 @@
+//
+// Class FieldWriter
+//   This class writes the bunch internal fields on the grid to
+//   file.
+//
+// Copyright (c) 2020, Paul Scherrer Institut, Villigen PSI, Switzerland
+// All rights reserved
+//
+// This file is part of OPAL.
+//
+// OPAL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with OPAL. If not, see <https://www.gnu.org/licenses/>.
+//
 #include <iomanip>
 #include <fstream>
 
