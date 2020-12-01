a9052ee7941189b6dd6601d190f4a5411a2533f5
ext-calvo_p <pedro.calvo@ciemat.es>
2020-12-01 10:56:43 +0100

    remove TYPE attribute from OpalCyclotron

--- src/Elements/OpalCyclotron.h
+++ src/Elements/OpalCyclotron.h
@@ -27,42 +27,37 @@ class OpalCyclotron: public OpalElement {
 public:
 
     /// The attributes of class OpalCyclotron.
-    /// Need to remove common, old TYPE = COMMON, prevents the identification of the fieldmap type in the Cyclotron
-    /// element issue #84
     enum {
-        TYPE,
-        GEOMETRY,  // geometry of boundary
-        CYHARMON,  // The harmonic number of the cyclotron
-        SYMMETRY,  // The symetry of the field
-        RINIT,     // The initial radius [mm]
-        PRINIT,    // The initial radial momentum [pr/p0] []
-        PHIINIT,   // The initial phase [deg]
-        ZINIT,     // The initial z coordinate [mm]
-        PZINIT,    // The initial vertical momentum [pz/p0] []
-        RFFREQ,    // First hamonic of the RF system [MHz]
-        FMAPFN,    // The filename of the mid-plane fieldmap
-        RFMAPFN,   // The filename(s) of the RF fieldmap
-        RFFCFN,    // The filename(s) of coefficients for RF frequency function f(t)
-        RFVCFN,    // The filename(s) of coefficients for RF voltage function v(t)
-        BSCALE,    // A scalar to scale the B-field
-        ESCALE,    // A scalar to scale the RF field
-        RFPHI,     // the initial phase of RF field
-        SUPERPOSE, // whether the electric field map are superposed or not
-        MINZ,      // minimal vertical extend of the machine
-        MAXZ,      // maximal vertical extend of the machine
-        MINR,      // minimal radial extend of the machine
-        MAXR,      // maximal radial extend of the machine
-        FMLOWE,    // minimal energy of the field map
-        FMHIGHE,   // maximal energy of the field map
-        SPIRAL,    // flag whether or not this is a spiral inflector simulation
-        TRIMCOILTHRESHOLD, // minimum B-field for which trim coils are applied
-        TRIMCOIL,  // list of trim coils
+        GEOMETRY = COMMON,  // geometry of boundary
+        CYHARMON,           // The harmonic number of the cyclotron
+        SYMMETRY,           // The symetry of the field
+        RINIT,              // The initial radius [mm]
+        PRINIT,             // The initial radial momentum [pr/p0] []
+        PHIINIT,            // The initial phase [deg]
+        ZINIT,              // The initial z coordinate [mm]
+        PZINIT,             // The initial vertical momentum [pz/p0] []
+        RFFREQ,             // First hamonic of the RF system [MHz]
+        FMAPFN,             // The filename of the mid-plane fieldmap
+        RFMAPFN,            // The filename(s) of the RF fieldmap
+        RFFCFN,             // The filename(s) of coefficients for RF frequency function f(t)
+        RFVCFN,             // The filename(s) of coefficients for RF voltage function v(t)
+        BSCALE,             // A scalar to scale the B-field
+        ESCALE,             // A scalar to scale the RF field
+        RFPHI,              // the initial phase of RF field
+        SUPERPOSE,          // whether the electric field map are superposed or not
+        MINZ,               // minimal vertical extend of the machine
+        MAXZ,               // maximal vertical extend of the machine
+        MINR,               // minimal radial extend of the machine
+        MAXR,               // maximal radial extend of the machine
+        FMLOWE,             // minimal energy of the field map
+        FMHIGHE,            // maximal energy of the field map
+        SPIRAL,             // flag whether or not this is a spiral inflector simulation
+        TRIMCOILTHRESHOLD,  // minimum B-field for which trim coils are applied
+        TRIMCOIL,           // list of trim coils
         SIZE
     };
 
 
-
-    /// Exemplar constructor.
     OpalCyclotron();
 
     virtual ~OpalCyclotron();
