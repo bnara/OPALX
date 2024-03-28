c9b28fbe06bbb0ad301ac24c8b6d7b4a5ee57ec8
Andreas Adelmann <andreas.adelmann@psi.ch>
2024-03-28 09:27:44 +0100

    cosmetics

--- build-script/setup.sh
+++ build-script/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
-export OTB_SRC_DIR=/psi/home/adelmann/opal-x/downloads
+export OTB_SRC_DIR=${HOME}/opal-x/downloads
+#
 # the build type will be appended by 401-build-opal 
-export OTB_PREFIX=/psi/home/adelmann/opal-x/install
-export HIK_INSTALL_DIR=$HOME/opal-x/ippl-build-scripts
+#
+export OTB_PREFIX=${HOME}/opal-x/install
+export HIK_INSTALL_DIR=${HOME}/opal-x/ippl-build-scripts
 export PATH=${OTB_SRC_DIR}/opal-x/build_openmp/src:${PATH}
