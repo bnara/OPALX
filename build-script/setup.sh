ef24d73586c0eaf703f726d1ec90812d0caeefe1
Andreas Adelmann <andreas.adelmann@psi.ch>
2024-03-28 09:24:00 +0100

    add opal to path

--- build-script/setup.sh
+++ build-script/setup.sh
@@ -3,3 +3,4 @@ export OTB_SRC_DIR=/psi/home/adelmann/opal-x/downloads
 # the build type will be appended by 401-build-opal 
 export OTB_PREFIX=/psi/home/adelmann/opal-x/install
 export HIK_INSTALL_DIR=$HOME/opal-x/ippl-build-scripts
+export PATH=${OTB_SRC_DIR}/opal-x/build_openmp/src:${PATH}
