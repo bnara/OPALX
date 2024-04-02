770333485d13f542f993454d110b32e502132146
Andreas Adelmann <andreas.adelmann@psi.ch>
2024-04-02 14:12:19 +0200

    remove unnessesary file

--- build-script/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-export OTB_SRC_DIR=${HOME}/opal-x/downloads
-#
-# the build type will be appended by 401-build-opal 
-#
-export OTB_PREFIX=${HOME}/opal-x/install
-export HIK_INSTALL_DIR=${HOME}/opal-x/ippl-build-scripts
-export PATH=${OTB_SRC_DIR}/opal-x/build_openmp/src:${PATH}
