a4a0f7229f4e6ec9b97cde504027107a7aace47a
Sadr Mohsen <sadr_m@merlin-l-001.psi.ch>
2024-09-12 13:46:16 +0200

    in progress

--- src/Distribution/FlatTop.hpp
+++ src/Distribution/FlatTop.hpp
@@ -38,28 +38,76 @@ public:
     Vector_t<double, 6> normMin_m;
     Vector_t<double, 6> normMax_m;
 
-    void generateUniformDisk(size_t& numberOfParticles) {
+    void generateUniformDisk(size_t& numberOfParticles, Vector_t<double, 3> nr) {
         GeneratorPool rand_pool64((size_t)(Options::seed + 100 * ippl::Comm->rank()));
+
+        double mu[3], sd[3];
+        Vector_t<double, 3> rmin;
+        Vector_t<double, 3> rmax;
+        Vector_t<double, 3> hr;
 
         view_type &Rview = pc_m->R.getView();
         view_type &Pview = pc_m->P.getView();
 
-       // TODO: sample (Rx,Ry) uniformly on a disk of radius.
-       // TODO: then, scale Rx and Ry with sigmaR_m[0] and sigmaR_m[1]
-       // TODO: set Px=Py=0
+        auto& mesh = fc_m->getMesh();
+        auto& FL = fc_m->getFL();
 
-/*
-        double rmin[3] = {0.0,   0.0, 0.0};
-        double rmax[3] = {1.0, 2.*pi, 0.0};
+        // STEP1: sample (Rx,Ry) uniformly on a unit disk.
+        // Here, we use Muller-Marsaglia method https://mathworld.wolfram.com/HyperspherePointPicking.html
+        // first sample normal distribution
+        for(int i=0; i<3; i++){
+            mu[i] = 0.0;
+            sd[i] = 1.0;
+        }
+        const double par[6] = {mu[0], sd[0], mu[1], sd[1], mu[2], sd[2]};
+        using Dist_t = ippl::random::NormalDistribution<double, Dim>;
+        using sampling_t = ippl::random::InverseTransformSampling<double, Dim, Kokkos::DefaultExecutionSpace, Dist_t>;
+        Dist_t dist(par);
 
-        ippl::random::randu<double, 3> randu_instance(Rview, rand_pool64, rmin, rmax);
+        hr = 6.0 / nr;
+        rmin = -3.0;
+        rmax = 3.0;
+        mesh.setMeshSpacing(hr);
+        mesh.setOrigin(rmin);
+        pc_m->getLayout().updateLayout(FL, mesh);
 
-        Kokkos::parallel_for(nlocal, ippl::random::randn<double, Dim>(position, rand_pool64, mu, sd)
-        );
+        ippl::detail::RegionLayout<double, Dim, Mesh_t<Dim>> rlayout;
+        rlayout = ippl::detail::RegionLayout<double, Dim, Mesh_t<Dim>>(FL, mesh);
 
+        sampling_t sampling(dist, rmax, rmin, rlayout, numberOfParticles);
+
+        size_type nlocal = sampling.getLocalSamplesNum();
+        pc_m->create(nlocal);
+
+        sampling.generate(Rview, rand_pool64);
+
+        // then bring (Rx,Ry) to a unit ring, and then unit disk
+        Kokkos::parallel_for(
+               "unitDisk", pc_m->getLocalNum(), KOKKOS_LAMBDA(const size_t j) {
+                double r = Kokkos::sqrt( Kokkos::pow( Rview(j)[0], 2 ) + Kokkos::pow( Rview(j)[1], 2 ) );
+                // (Rx,Ry) to a unit ring
+                Rview(j)[0] /= r;
+                Rview(j)[1] /= r;
+                Rview(j)[2]  = 0.0;
+                // (Rx,Ry) to a unit disk
+                auto generator = rand_pool64.get_state();
+                double scale = generator.drand(0., 1.);
+                Rview(j)[0] *= scale;
+                Rview(j)[1] *= scale;
+                rand_pool64.free_state(generator);
+        });
         Kokkos::fence();
-        ippl::Comm->barrier();
-*/
+
+       // STEP2: scale Rx and Ry with sigmaRx and sigmaRy. Also, set Px=Py=0.
+        Vector_t<double, 3> sigmaR = opalDist_m->getSigmaR();
+        Kokkos::parallel_for(
+               "scaleRxRy", pc_m->getLocalNum(), KOKKOS_LAMBDA(const size_t j) {
+               Rview(j)[0] *= sigmaR[0];
+               Rview(j)[1] *= sigmaR[1];
+               Pview(j)[0] = 0.0;
+               Pview(j)[1] = 0.0;
+        });
+	Kokkos::fence();
 
     }
 
@@ -71,13 +119,59 @@ public:
 
        // TODO: sample particle time according to the flat top profile
        // TODO: set Rz=Pz to zero
+
+
+        double flattopTime = opalDist_m->getTPulseLengthFWHM()
+             - std::sqrt(2.0 * std::log(2.0)) * (opalDist_m->getSigmaTRise() + opalDist_m->getSigmaTFall());
+
+        if (flattopTime < 0.0)
+              flattopTime = 0.0;
+
+        double normalizedFlankArea = 0.5 * std::sqrt(Physics::two_pi) * gsl_sf_erf(opalDist_m->getCutoffR()[2] / std::sqrt(2.0));
+        double distArea = flattopTime
+                + (getSigmaTRise() + opalDist_m->getSigmaTFall()) * normalizedFlankArea;
+
+        // Find number of particles in rise, fall and flat top.
+        size_t numRise = numberOfParticles * opalDist_m->getSigmaTRise() * normalizedFlankArea / distArea;
+        size_t numFall = numberOfParticles * opalDist_m->getSigmaTFall() * normalizedFlankArea / distArea;
+        size_t numFlat = numberOfParticles - numRise - numFall;
+
+        // Generate particles in tail.
+        int saveProcessor = -1;
+        const int myNode = Ippl::myNode();
+        const int numNodes = Ippl::getNodes();
+        const bool scalable = Attributes::getBool(itsAttr[Attrib::Distribution::SCALABLE]);
+/*
+        for (size_t partIndex = 0; partIndex < numFall; partIndex++) {
+
+           double t = 0.0;
+           double pz = 0.0;
+
+          bool allow = false;
+          while (!allow) {
+              t = gsl_ran_gaussian_tail(randGen_m, 0, sigmaTFall_m);
+              if (t <= sigmaTFall_m * cutoffR_m[2]) {
+                  t = -t + sigmaTFall_m * cutoffR_m[2];
+                  allow = true;
+              }
+          }
+
+          // Save to each processor in turn.
+          saveProcessor++;
+          if (saveProcessor >= numNodes)
+              saveProcessor = 0;
+
+          if (scalable || myNode == saveProcessor) {
+              tOrZDist_m.push_back(t);
+          }
+*/
     }
 
     void generateParticles(size_t& numberOfParticles, Vector_t<double, 3> nr) override {
 
         GeneratorPool rand_pool64((size_t)(Options::seed + 100 * ippl::Comm->rank()));
 
-        generateUniformDisk(numberOfParticles);
+        generateUniformDisk(numberOfParticles, nr);
 
         generateLongFlattopT(numberOfParticles);
     }
