2980e8f55dbd925ce2e9e250c1102432c4ee57e8
Alexander Liemen <alexander.liemen@psi.ch>
2025-01-26 15:06:47 +0100

    Fix E field averaging for empty simulations to avoid /0

--- src/PartBunch/PartBunch.cpp
+++ src/PartBunch/PartBunch.cpp
@@ -75,7 +75,8 @@ void PartBunch<double,3>::spaceChargeEFieldCheck() {
  MPI_Reduce(myRank == 0 ? MPI_IN_PLACE : &minE, &minE, 1, MPI_DOUBLE, MPI_MIN, 0, ippl::Comm->getCommunicator());
  MPI_Reduce(myRank == 0 ? MPI_IN_PLACE : &maxE, &maxE, 1, MPI_DOUBLE, MPI_MAX, 0, ippl::Comm->getCommunicator());
  
- avgE /= this->getTotalNum();
+ size_t Np = this->getTotalNum();
+ avgE /= (Np == 0) ? 1 : Np; // avoid division by zero for empty simulations (see also DistributionMoments::computeMeans implementation) 
  msg << "avgENorm = " << avgE << endl;
  msg << "minEComponent = " << minEComponent << endl;
  msg << "maxEComponent = " << maxEComponent << endl;
