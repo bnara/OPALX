7a8fcbb0f3030c3967515a823094149ef9036b83
Mohsen Sadr <mohsen.sadr91@gmail.com>
2025-11-24 15:30:30 +0100

    debugged the test case.

--- unit_tests/Distribution/TestGaussian.cpp
+++ unit_tests/Distribution/TestGaussian.cpp
@@ -4,118 +4,124 @@
 #include <cmath>
 
 #include "Distribution/Gaussian.h"
-#include "Distribution/Distribution.h"
-#include "PartBunch/FieldContainer.hpp"
-#include "PartBunch/ParticleContainer.hpp"
+#include "Ippl.h"
+#include "Utility/IpplTimings.h"
 
-using ParticleContainer_t = ParticleContainer<double, 3>;
-using FieldContainer_t = FieldContainer<double, 3>;
-using Distribution_t = Distribution;
+template <typename T>
+using ParticleAttrib = ippl::ParticleAttrib<T>;
+
+using size_type = ippl::detail::size_type;
+
+// a minimal ParticleContainer definition for testing
+template <typename T, unsigned Dim>
+class ParticleContainer_min : public ParticleContainer<T, Dim> {
+    using Base = ParticleContainer<T, Dim>;
+public:
+    /// particle momenta [\beta\gamma]
+    typename Base::particle_position_type P;
+
+    ParticleContainer_min(Mesh_t<Dim>& mesh, FieldLayout_t<Dim>& FL)
+        : Base(mesh, FL)  // initialize the base class
+    {
+        registerAttributes();
+    }
+
+    ~ParticleContainer_min() = default;
+
+    void registerAttributes() {
+        this->addAttribute(P);
+    }
+};
 
 class GaussianTest : public ::testing::Test {
 protected:
     static void SetUpTestSuite() {
-        // Initialize MPI if not already initialized
-        int mpi_initialized = 0;
-        MPI_Initialized(&mpi_initialized);
-        if (!mpi_initialized) {
-            MPI_Init(nullptr, nullptr);
-            mpi_init_here = true;
-        }
+        int argc = 0;
+        char** argv = nullptr;
 
-        // Initialize Kokkos (required before creating any Kokkos::View)
-        Kokkos::initialize();
-        kokkos_initialized_here = true;
+        ippl::initialize(argc, argv);
     }
 
     static void TearDownTestSuite() {
-        // Finalize Kokkos
-        if (kokkos_initialized_here) {
-            Kokkos::finalize();
-            kokkos_initialized_here = false;
-        }
-
-        // Finalize MPI if we initialized it
-        if (mpi_init_here) {
-            MPI_Finalize();
-            mpi_init_here = false;
-        }
+        ippl::finalize();
     }
 
     void SetUp() override {
         // Minimal 3D grid parameters
-        ippl::Vector<double,3> hr;     hr(0) = 1.0; hr(1) = 1.0; hr(2) = 1.0;
-        ippl::Vector<double,3> rmin;   rmin(0) = -10.0; rmin(1) = -10.0; rmin(2) = -10.0;
-        ippl::Vector<double,3> rmax;   rmax(0) = 10.0;  rmax(1) = 10.0;  rmax(2) = 10.0;
-        ippl::Vector<double,3> origin; origin(0) = 0.0;  origin(1) = 0.0;  origin(2) = 0.0;
-        std::array<bool,3> decomp = {false, false, false};
+        nr = 64;
+        ippl::Vector<double,3> rmin = -4.0;
+        ippl::Vector<double,3> rmax = 4.0;
+        ippl::Vector<double,3> origin = rmin;
+        ippl::Vector<double,3> hr = (rmax - rmin) / ippl::Vector<double,3>(nr);
+        std::array<bool,3> decomp = {true, true, true};
 
-        ippl::NDIndex<3> domain;  // default constructor
-        domain[0] = 4;
-        domain[1] = 4;
-        domain[2] = 4;
+        ippl::NDIndex<3> domain;
+        for (unsigned i = 0; i < 3; i++) {
+            domain[i] = ippl::Index(this->nr[i]);
+        }
 
         // Create FieldContainer
-        fc = std::make_shared<FieldContainer_t>(hr, rmin, rmax, decomp, domain, origin, true);
+        auto fc = std::make_shared<FieldContainer_t>(hr, rmin, rmax, decomp, domain, origin, this->isAllPeriodic_m);
 
         // Create mesh and fieldlayout
         Mesh_t<3> mesh(domain, hr, origin);
-        FieldLayout_t<3> fl(MPI_COMM_WORLD, domain, decomp, true);
+        FieldLayout_t<3> fl(MPI_COMM_WORLD, domain, decomp, this->isAllPeriodic_m);
 
         // Create ParticleContainer
-        pc = std::make_shared<ParticleContainer_t>(mesh, fl);
-
-        // Create Distribution
-        dist = std::make_shared<Distribution_t>();
+        pc = std::make_shared<ParticleContainer_min<double, 3>>(mesh, fl);
     }
 
     void TearDown() override {
         // nothing special
     }
 
-    static inline bool mpi_init_here = false;
-    static inline bool kokkos_initialized_here = false;
-
-    std::shared_ptr<ParticleContainer_t> pc;
-    std::shared_ptr<FieldContainer_t> fc;
-    std::shared_ptr<Distribution_t> dist;
+    std::shared_ptr<ParticleContainer_min<double, 3>> pc;
+    ippl::Vector<int,3> nr;
+    bool isAllPeriodic_m = true;
 };
 
-TEST_F(GaussianTest, GenerateParticlesBasic) {
-    Gaussian sampler(pc, fc, dist);
+TEST_F(GaussianTest, meanR) {
+    const Vector_t<double, 3> sigmaR = 1.0;
+    const Vector_t<double, 3> sigmaP = 1.0;
+    double avrgpz = 0.0;
+    const Vector_t<double, 3> cutoffR = 3.0;
 
-    size_t N = 1000;
-    ippl::Vector<double,3> nr; nr(0)=1; nr(1)=1; nr(2)=1;
+    Gaussian sampler(pc,
+                    sigmaR, sigmaP, avrgpz, cutoffR);
 
-    sampler.generateParticles(N, nr);
+    size_t total_nparticles = 1000000;
+
+    sampler.generateParticles(total_nparticles, nr);
 
     auto Rview = pc->R.getView();
     auto Pview = pc->P.getView();
 
-    auto R_h = Kokkos::create_mirror_view(Rview);
-    auto P_h = Kokkos::create_mirror_view(Pview);
-    Kokkos::deep_copy(R_h, Rview);
-    Kokkos::deep_copy(P_h, Pview);
-
-    size_t nlocal = R_h.extent(0);
-    ASSERT_GT(nlocal, 0u);
-
-    double meanRx=0, meanRy=0, meanRz=0, meanPz=0;
-    for(size_t i=0;i<nlocal;i++){
-        meanRx += R_h(i)[0];
-        meanRy += R_h(i)[1];
-        meanRz += R_h(i)[2];
-        meanPz  += P_h(i)[2];
+    // compute mean of R
+    double meanR[3], sumR[3];
+    for(int i=0; i<3; i++){
+        meanR[i] = 0.0;
+        sumR[i] = 0.0;
     }
-    meanRx /= nlocal;
-    meanRy /= nlocal;
-    meanRz /= nlocal;
-    meanPz  /= nlocal;
 
-    EXPECT_NEAR(meanRx, 0.0, 1e-1);
-    EXPECT_NEAR(meanRy, 0.0, 1e-1);
-    EXPECT_NEAR(meanRz, 0.0, 1e-1);
+    Kokkos::parallel_reduce(
+            "calc moments of particle distr.", pc->getLocalNum(),
+            KOKKOS_LAMBDA(
+                    const int k, double& cent0, double& cent1, double& cent2) {
+                    cent0 += Rview(k)[0];
+                    cent1 += Rview(k)[1];
+                    cent2 += Rview(k)[2];
+            },
+            Kokkos::Sum<double>(sumR[0]), Kokkos::Sum<double>(sumR[1]), Kokkos::Sum<double>(sumR[2]));
+    Kokkos::fence();
 
-    double avrgpz = dist->getAvrgpz();
-    EXPECT_NEAR(meanPz, avrgpz, 1e-1);
+    MPI_Allreduce(sumR, meanR, 3, MPI_DOUBLE, MPI_SUM, ippl::Comm->getCommunicator());
+    ippl::Comm->barrier();
+
+    for(int i=0; i<3; i++){
+       meanR[i] = meanR[i]/(1.0*total_nparticles);
+    }
+
+    EXPECT_NEAR(meanR[0], 0.0, 1e-15);
+    EXPECT_NEAR(meanR[1], 0.0, 1e-15);
+    EXPECT_NEAR(meanR[2], 0.0, 1e-15);
 }
