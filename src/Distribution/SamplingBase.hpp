881a6e4549ac2ad45841a52866c6aa9016e6ae58
Mohsen <mohsensadr@gmail.com>
2025-12-20 17:06:43 +0100

    some cleanup

--- src/Distribution/SamplingBase.hpp
+++ src/Distribution/SamplingBase.hpp
@@ -35,19 +35,19 @@ public:
     
     virtual ~SamplingBase() {}
 
-    virtual void generateParticles(size_t& numberOfParticles, Vector_t<double, 3> nr) {}
+    virtual void generateParticles(size_t& /*numberOfParticles*/, Vector_t<double, 3> /*nr*/) {}
 
-    virtual void emitParticles(double t, double dt) {}
+    virtual void emitParticles(double /*t*/, double /*dt*/) {}
 
     // testNumEmitParticles is purely made for testing and should be removed
-    virtual void testNumEmitParticles(size_t nsteps, double dt) {}
+    virtual void testNumEmitParticles(size_t /*nsteps*/, double /*dt*/) {}
 
     // testEmitParticles is purely made for testing and should be removed
-    virtual void testEmitParticles(size_t nsteps, double dt) {}
+    virtual void testEmitParticles(size_t /*nsteps*/, double /*dt*/) {}
 
-    virtual void initDomainDecomp(double BoxIncr) {}
+    virtual void initDomainDecomp(double /*BoxIncr*/) {}
 
-    virtual void setWithDomainDecomp(bool withDomainDecomp) {}
+    virtual void setWithDomainDecomp(bool /*withDomainDecomp*/) {}
 };
 #endif
 
