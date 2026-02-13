ff6feb04db066332ae5049432ab19ce35dafd517
jonathan <jon.thompson@stellarinception.com>
2026-02-13 17:27:14 +0000

    MultipoleT should not be configured yet.

--- src/OpalConfigure/Configure.cpp
+++ src/OpalConfigure/Configure.cpp
@@ -134,7 +134,7 @@ namespace {
         opal->create(new OpalPolynomialTimeDependence());
         opal->create(new OpalSinusoidalTimeDependence());
         opal->create(new OpalSplineTimeDependence());
-        opal->create(new OpalMultipoleT());
+        //opal->create(new OpalMultipoleT());
     }
 };  // namespace
 
