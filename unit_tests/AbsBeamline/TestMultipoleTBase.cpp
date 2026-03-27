c292f441e2519053aa9d56744b38a1981cc3fece
jonathan <jon.thompson@stellarinception.com>
2026-03-27 09:18:14 +0000

    Removed the death tests.

--- unit_tests/AbsBeamline/TestMultipoleTBase.cpp
+++ unit_tests/AbsBeamline/TestMultipoleTBase.cpp
@@ -53,6 +53,7 @@ protected:
     }
 };
 
+#if 0
 TEST_F(MultipoleTBaseTest, FactorialBoundaryCondition) {
     GTEST_FLAG_SET(death_test_style, "threadsafe");
     EXPECT_DEATH(MultipoleTBase::factorial(MaxFactorial + 1), "factorial out of bounds");
@@ -63,6 +64,7 @@ TEST_F(MultipoleTBaseTest, PowerIntegerBoundaryCondition) {
     EXPECT_DEATH(
         MultipoleTBase::powerInteger(5, MaxPowerInteger + 1), "integer power out of bounds");
 }
+#endif
 
 TEST_F(MultipoleTBaseTest, TransverseDerivatives) {
     constexpr Kokkos::Array poles = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
