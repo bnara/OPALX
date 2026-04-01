45824a6e3639b80fc4b7619640359629c8422297
Alexander Liemen <47395086+aliemen@users.noreply.github.com>
2026-04-01 08:58:50 +0200

    Add zerofacebc to emissionsource structure

--- src/Structure/EmissionSource.cpp
+++ src/Structure/EmissionSource.cpp
@@ -4,11 +4,12 @@
 #include "Utilities/OpalException.h"
 
 EmissionSource::EmissionSource()
-    : Definition(SIZE, "EMISSIONSOURCE",
-                 "The EMISSIONSOURCE statement defines a particle injector with "
-                 "position R0, momentum P0, start time t0, and a linked Distribution.") {
+    : Definition(
+              SIZE, "EMISSIONSOURCE",
+              "The EMISSIONSOURCE statement defines a particle injector with "
+              "position R0, momentum P0, start time t0, and a linked Distribution.") {
     itsAttr[DISTRIBUTION] =
-        Attributes::makeString("DISTRIBUTION", "Name of the Distribution to sample from.");
+            Attributes::makeString("DISTRIBUTION", "Name of the Distribution to sample from.");
 
     itsAttr[R0X] = Attributes::makeReal("R0X", "Initial position x [m].", 0.0);
     itsAttr[R0Y] = Attributes::makeReal("R0Y", "Initial position y [m].", 0.0);
@@ -19,6 +20,10 @@ EmissionSource::EmissionSource()
     itsAttr[P0Z] = Attributes::makeReal("P0Z", "Initial momentum z [beta*gamma].", 0.0);
 
     itsAttr[T0] = Attributes::makeReal("T0", "Start time when sampling begins [s].", 0.0);
+
+    itsAttr[ZEROFACE_R0Z] = Attributes::makeBool(
+            "ZEROFACE_R0Z", "Set Dirichlet boundary conditions (0 potential) in xy plane at R0Z.",
+            false);
 
     registerOwnership(AttributeHandler::STATEMENT);
 }
@@ -40,7 +45,7 @@ void EmissionSource::execute() {}
 
 EmissionSource* EmissionSource::find(const std::string& name) {
     Object* obj = OpalData::getInstance()->find(name);
-    auto* es   = dynamic_cast<EmissionSource*>(obj);
+    auto* es    = dynamic_cast<EmissionSource*>(obj);
     if (es == nullptr) {
         throw OpalException("EmissionSource::find()", "EmissionSource \"" + name + "\" not found.");
     }
@@ -52,17 +57,17 @@ std::string EmissionSource::getDistributionName() const {
 }
 
 ippl::Vector<double, 3> EmissionSource::getR0() const {
-    return ippl::Vector<double, 3>(Attributes::getReal(itsAttr[R0X]),
-                                    Attributes::getReal(itsAttr[R0Y]),
-                                    Attributes::getReal(itsAttr[R0Z]));
+    return ippl::Vector<double, 3>(
+            Attributes::getReal(itsAttr[R0X]), Attributes::getReal(itsAttr[R0Y]),
+            Attributes::getReal(itsAttr[R0Z]));
 }
 
 ippl::Vector<double, 3> EmissionSource::getP0() const {
-    return ippl::Vector<double, 3>(Attributes::getReal(itsAttr[P0X]),
-                                    Attributes::getReal(itsAttr[P0Y]),
-                                    Attributes::getReal(itsAttr[P0Z]));
+    return ippl::Vector<double, 3>(
+            Attributes::getReal(itsAttr[P0X]), Attributes::getReal(itsAttr[P0Y]),
+            Attributes::getReal(itsAttr[P0Z]));
 }
 
-double EmissionSource::getT0() const {
-    return Attributes::getReal(itsAttr[T0]);
-}
+double EmissionSource::getT0() const { return Attributes::getReal(itsAttr[T0]); }
+
+bool EmissionSource::getZeroFaceR0Z() const { return Attributes::getBool(itsAttr[ZEROFACE_R0Z]); }
